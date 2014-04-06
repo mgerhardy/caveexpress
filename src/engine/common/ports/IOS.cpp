502c5f01a77c0712b9c3ddd2df6dcd7d7bf9e6ed
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-04-06 19:54:31 +0200

    * ios port second part

---
+++ src/engine/common/ports/IOS.cpp
@@ -0,0 +1,43 @@
+#include "IOS.h"
+#include "Cocoa.h"
+#include "engine/common/Version.h"
+
+IOS::IOS () :
+		Unix()
+{
+	nsinit();
+}
+
+IOS::~IOS ()
+{
+}
+
+void IOS::logError (const std::string& error) const
+{
+	nslogOutput("ERROR: " + error);
+}
+
+void IOS::logOutput (const std::string& string) const
+{
+	nslogOutput(string);
+}
+
+std::string IOS::getHomeDirectory ()
+{
+	char* home = nsGetHomeDirectory(APPNAME);
+	if (home == nullptr)
+		return "";
+	return home;
+}
+
+void IOS::showAds (bool show, bool ontop)
+{
+	// TODO:
+}
+
+int IOS::openURL (const std::string& url) const
+{
+	const std::string cmd = "open " + url;
+	system(cmd.c_str());
+    return 0;
+}
---
+++ src/engine/common/ports/IOS.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "engine/common/ports/Unix.h"
+
+class IOS: public Unix {
+public:
+	IOS ();
+	virtual ~IOS ();
+
+public:
+	void logError (const std::string& error) const override;
+	void logOutput (const std::string& string) const override;
+	void showAds (bool show, bool ontop) override;
+	int openURL (const std::string& url) const override;
+	std::string getHomeDirectory () override;
+};
+
