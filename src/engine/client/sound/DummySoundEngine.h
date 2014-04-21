aa96ec6e190954d963512bff22bb4ee10e3e5561
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-04-21 16:45:36 +0200

    * added missing overrides

--- src/engine/client/sound/DummySoundEngine.h
+++ src/engine/client/sound/DummySoundEngine.h
@@ -11,11 +11,11 @@ public:
 	bool init (bool initCache) override {return false;}
 	void close () override {}
 	bool exists (const std::string& sound) const override { return false; }
-	void halt (int sound) {}
-	bool cache (const std::string& sound) { return false; }
-	void haltAll () {}
-	void pause () {}
-	void resume () {}
+	void halt (int sound) override {}
+	bool cache (const std::string& sound) override { return false; }
+	void haltAll () override {}
+	void pause () override {}
+	void resume () override {}
 	int playMusic (const std::string& music, bool loop) override { return -1; }
 	void haltMusic (int music) override {}
 	int play (const std::string& filename, const vec2& position, bool loop) override { return -1; }
