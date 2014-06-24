1f274d7a9c955da545e7a94edf9a4759b9a89022
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-06-24 19:21:55 +0200

    * make sure to set the sdl related cflags to 1 - defining them is not enough

--- build/platforms/nacl.mk
+++ build/platforms/nacl.mk
@@ -12,10 +12,9 @@ NACL_CFG                 ?= $(NACL_SDK_ROOT)/tools/nacl_config.py
 NACL_CFLAGS              := $(shell $(NACL_CFG) -t pnacl --cflags)
 NACL_LDFLAGS             := $(shell $(NACL_CFG) -t pnacl --lib)
 
-CFLAGS                   += -D_GNU_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE -DSQLITE_OMIT_LOAD_EXTENSION -DSDL_VIDEO_DRIVER_NACL -DSDL_AUDIO_DRIVER_NACL -DSDL_FILESYSTEM_NACL -DNONETWORK -DHAVE_POW=1 -DHAVE_OPENGLES2=1 -DSDL_VIDEO_OPENGL_ES2=1 -DSDL_VIDEO_RENDER_OGL_ES2=1 -DHAVE_MALLOC -DHAVE_STDIO_H $(NACL_CFLAGS)
+CFLAGS                   += -D_GNU_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE -DSQLITE_OMIT_LOAD_EXTENSION -DSDL_VIDEO_DRIVER_NACL=1 -DSDL_AUDIO_DRIVER_NACL=1 -DSDL_FILESYSTEM_NACL=1 -DNONETWORK -DHAVE_POW=1 -DHAVE_OPENGLES2=1 -DSDL_VIDEO_OPENGL_ES2=1 -DSDL_VIDEO_RENDER_OGL_ES2=1 -DHAVE_MALLOC=1 -DHAVE_STDIO_H=1 $(NACL_CFLAGS)
 
-LDFLAGS                  += $(NACL_LDFLAGS)/pnacl/Release -lppapi -lppapi_simple -lppapi_gles2 -lppapi_cpp -lnacl_io
-SDL_LIBS                 += -lpthread
+LDFLAGS                  += $(NACL_LDFLAGS)/pnacl/Release -lppapi_simple -lnacl_io -lppapi_cpp -lppapi -lpthread -lppapi_gles2
 OPENGL_LIBS               =
 SDL_NET_SRCS              =
 
