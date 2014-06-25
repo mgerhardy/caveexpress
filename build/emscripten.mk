ff9d0b1e01bfc97d91953d5bd11bd640f3b1f846
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-06-25 23:00:54 +0200

    * export the emscripten env vars in the setup target

--- build/emscripten.mk
+++ build/emscripten.mk
@@ -7,4 +7,6 @@ emscripten-setup:
 	git clone https://github.com/kripken/emscripten-fastcomp-clang $(EMSCRIPTEN_TARGET_ROOT)/emscripten-fastcomp/tools/clang; \
 	mkdir -p $(EMSCRIPTEN_TARGET_ROOT)/emscripten-fastcomp/build; \
 	cd $(EMSCRIPTEN_TARGET_ROOT)/emscripten-fastcomp/build && ../configure --enable-optimized --disable-assertions --enable-targets=host,js; make -j 2; \
-	echo "LLVM_ROOT=$(EMSCRIPTEN_TARGET_ROOT)/emscripten-fastcom/build/bin/Release" >> ~/.emscripten
+	$(Q)echo "Configure paths..."; \
+	echo "export LLVM_ROOT=\"$(EMSCRIPTEN_TARGET_ROOT)/emscripten-fastcom/build/bin/Release\"" >> ~/.bashrc
+	echo "export EMSCRIPTEN=\"$(EMSCRIPTEN_TARGET_ROOT)/emscripten\"" >> ~/.bashrc
