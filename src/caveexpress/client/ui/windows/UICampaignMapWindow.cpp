f691d3b57f0a6fcac5bb3d079b08481d8d6dd501
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-07-14 21:17:03 +0200

    * moved into engine space

--- src/caveexpress/client/ui/windows/UICampaignMapWindow.cpp
+++ src/caveexpress/client/ui/windows/UICampaignMapWindow.cpp
@@ -2,7 +2,7 @@
 #include "engine/client/ui/nodes/UINodeMapSelector.h"
 #include "engine/client/ui/nodes/UINodeButton.h"
 #include "engine/common/campaign/CampaignManager.h"
-#include "caveexpress/client/ui/windows/campaignmapwindow/ResetCampaignListener.h"
+#include "engine/client/ui/windows/campaignmapwindow/ResetCampaignListener.h"
 
 UICampaignMapWindow::UICampaignMapWindow (IFrontend *frontend, CampaignManager &campaignManager) :
 	UIMapSelectorWindow(new UINodeMapSelector(frontend, campaignManager), tr("Maps"), UI_WINDOW_CAMPAIGN_MAPS, frontend, WINDOW_FLAG_FULLSCREEN | WINDOW_FLAG_MAIN)
