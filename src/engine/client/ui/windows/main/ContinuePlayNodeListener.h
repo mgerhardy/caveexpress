f4999eb0f2893e3882891dec8ab0e56d2136c645
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-07-21 16:45:12 +0200

    * logging

--- src/engine/client/ui/windows/main/ContinuePlayNodeListener.h
+++ src/engine/client/ui/windows/main/ContinuePlayNodeListener.h
@@ -5,6 +5,7 @@
 #include "engine/client/ui/windows/UIWindow.h"
 #include "engine/common/ServiceProvider.h"
 #include "engine/common/network/INetwork.h"
+#include "engine/common/Logger.h"
 
 class ContinuePlayNodeListener: public UINodeListener {
 private:
@@ -18,14 +19,17 @@ public:
 
 	void onClick () override
 	{
+		info(LOG_CLIENT, "Continue in campaign");
 		if (_serviceProvider.getNetwork().isMultiplayer()) {
 			UI::get().pop();
 			return;
 		}
 		if (_campaignManager.isNewlyCompleted()) {
+			info(LOG_CLIENT, "Finished the campaign");
 			UI::get().popMain();
 			UI::get().push(UI_WINDOW_GAMEFINISHED);
 		} else {
+			info(LOG_CLIENT, "Continue play");
 			UI::get().pop();
 			_campaignManager.continuePlay();
 		}
