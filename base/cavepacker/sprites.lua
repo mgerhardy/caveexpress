546035722da6b374f99edc96ff4a46ccae1070ab
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-07-21 16:44:53 +0200

    * updated sprites

--- base/cavepacker/sprites.lua
+++ base/cavepacker/sprites.lua
@@ -8,24 +8,6 @@ sprites = {
 	["player-idle"] = {
 	},
 
--- NPC Woman
-
-	["npc-woman-walk"] = {
-		fps = 12,
-	},
-
--- NPC Grandpa
-
-	["npc-grandpa-walk"] = {
-		fps = 12,
-	},
-
--- NPC Man
-
-	["npc-man-walk"] = {
-		fps = 12,
-	},
-
 -- Package
 
 	["item-package-idle"] = {
@@ -33,42 +15,16 @@ sprites = {
 		theme = "rock",
 	},
 
--- Ice Tiles
-
-	["tile-background-ice-01"] = { type = "background", theme = "ice", },
-	["tile-background-ice-02"] = { type = "background", theme = "ice", },
-	["tile-background-ice-03"] = { type = "background", theme = "ice", },
-	["tile-background-ice-04"] = { type = "background", theme = "ice", },
-	["tile-background-ice-05"] = { type = "background", theme = "ice", },
-	["tile-background-ice-06"] = { type = "background", theme = "ice", },
-	["tile-background-ice-07"] = { type = "background", theme = "ice", },
-	["tile-background-ice-08"] = { type = "background", theme = "ice", },
-	["tile-background-ice-big-01"] = { type = "background", width = 2, height = 2, theme = "ice", },
-	["tile-ground-ice-01"] = { type = "ground", theme = "ice", },
-	["tile-ground-ice-02"] = { type = "ground", theme = "ice", },
-	["tile-ground-ice-03"] = { type = "ground", theme = "ice", },
-	["tile-ground-ice-04"] = { type = "ground", theme = "ice", },
-	["tile-ground-ice-06"] = { type = "ground", theme = "ice", },
-	["tile-ground-ice-big-01"] = { type = "ground", width = 2, height = 2, theme = "ice", },
-	["tile-rock-ice-01"] = { type = "rock", theme = "ice", },
-	["tile-rock-ice-02"] = { type = "rock", theme = "ice", },
-	["tile-rock-ice-03"] = { type = "rock", theme = "ice", },
-	["tile-rock-ice-big-01"] = { type = "rock", width = 2, height = 2, theme = "ice", },
-
 -- Rock Tiles
 
-	["tile-background-01"] = { type = "background", theme = "rock", },
-	["tile-background-02"] = { type = "background", theme = "rock", },
-	["tile-background-03"] = { type = "background", theme = "rock", },
-	["tile-background-04"] = { type = "background", theme = "rock", },
-	["tile-background-big-01"] = { type = "background", width = 2, height = 2, theme = "rock", },
-	["tile-ground-01"] = { type = "ground", theme = "rock", },
-	["tile-ground-02"] = { type = "ground", theme = "rock", },
-	["tile-ground-03"] = { type = "ground", theme = "rock", },
-	["tile-ground-04"] = { type = "ground", theme = "rock", },
+	["tile-background-01"] = { type = "ground", theme = "rock", },
+	["tile-background-02"] = { type = "ground", theme = "rock", },
+	["tile-background-03"] = { type = "ground", theme = "rock", },
+	["tile-background-04"] = { type = "ground", theme = "rock", },
+	["tile-background-big-01"] = { type = "ground", width = 2, height = 2, theme = "rock", },
 
-	["tile-rock-01"] = { type = "rock", theme = "rock", },
-	["tile-rock-02"] = { type = "rock", theme = "rock", },
-	["tile-rock-03"] = { type = "rock", theme = "rock", },
-	["tile-rock-big-01"] = { type = "rock", width = 2, height = 2, theme = "rock", },
+	["tile-rock-01"] = { type = "solid", theme = "rock", },
+	["tile-rock-02"] = { type = "solid", theme = "rock", },
+	["tile-rock-03"] = { type = "solid", theme = "rock", },
+	["tile-rock-big-01"] = { type = "solid", width = 2, height = 2, theme = "rock", },
 }
