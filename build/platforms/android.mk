857634156179e567464c905a4625822248d23310
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-07-24 21:40:57 +0200

    * hopefully fixed exclude paths

--- build/platforms/android.mk
+++ build/platforms/android.mk
@@ -1,3 +1,7 @@
 MIN_ANDROID_SDK=10
 TARGET_ANDROID_SDK=13
-EXCLUDE_JAVA_PACKAGE_PATHS:=**/org/caveexpressouya/**
+ifeq ($(APPNAME),cavepacker)
+    EXCLUDE_JAVA_PACKAGE_PATHS:=**/org/caveexpress*/** **/org/*ouya/**
+else
+    EXCLUDE_JAVA_PACKAGE_PATHS:=**/org/cavepacker*/** **/org/*ouya/**
+endif
--- build/platforms/ouya.mk
+++ build/platforms/ouya.mk
@@ -1,4 +1,8 @@
 MIN_ANDROID_SDK=16
 TARGET_ANDROID_SDK=16
 CONFIG_H:=android
-EXCLUDE_JAVA_PACKAGE_PATHS:=**/org/caveexpress/** **/org/caveexpresshd/** **/org/base/game/**
+ifeq ($(APPNAME),cavepacker)
+    EXCLUDE_JAVA_PACKAGE_PATHS:=**/org/caveexpress*/** **/org/base/game/**
+else
+    EXCLUDE_JAVA_PACKAGE_PATHS:=**/org/cavepacker*/** **/org/base/game/**
+endif
