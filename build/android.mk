e4c9b433d38276629cf338e5c81caa610457be2f
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-07-24 22:00:14 +0200

    * allow to compile android with and without network support

--- build/android.mk
+++ build/android.mk
@@ -108,6 +108,7 @@ $(ANDROID_PROJECT)/AndroidManifest.xml: $(SRCDIR)/Android.mk.in $(ANDROID_PROJEC
 	$(Q)sed -i 's/@APPLICATION_MK@/$(APPLICATION_MK)/g' $(ANDROID_PROJECT)/jni/Application.mk
 	$(Q)cp $(SRCDIR)/Android.mk.in $(SRCDIR)/Android.mk
 	$(Q)sed -i 's/@APPNAME@/$(APPNAME)/g' $(SRCDIR)/Android.mk
+	$(Q)sed -i 's/@OWN_CFLAGS@/$(SDL_NET_CFLAGS)/g' $(SRCDIR)/Android.mk
 	$(Q)sed -i 's/import org.caveexpress.*R;/import $(JAVA_PACKAGE).R;/g' $(ANDROID_PROJECT)/src/org/base/game/GameHelper.java
 	$(Q)sed -i 's/import org.caveexpress.*R;/import $(JAVA_PACKAGE).R;/g' $(ANDROID_PROJECT)/src/org/base/game/GameHelperUtils.java
 
