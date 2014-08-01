057f5ae32211bb4914838d5422af090a3056837a
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-08-01 19:37:34 +0200

    * use PROTOCOL_CLASS_SIMPLE for AutoSolveStartedMessage

--- src/cavepacker/shared/network/messages/AutoSolveStartedMessage.h
+++ src/cavepacker/shared/network/messages/AutoSolveStartedMessage.h
@@ -3,20 +3,4 @@
 #include "engine/common/network/IProtocolMessage.h"
 #include "cavepacker/shared/network/ProtocolMessageTypes.h"
 
-class AutoSolveStartedMessage: public IProtocolMessage {
-public:
-	AutoSolveStartedMessage () :
-			IProtocolMessage(protocol::PROTO_AUTOSOLVE)
-	{
-	}
-
-	AutoSolveStartedMessage (ByteStream& input) :
-			IProtocolMessage(protocol::PROTO_AUTOSOLVE)
-	{
-	}
-
-	void serialize (ByteStream& out) const override
-	{
-		out.addByte(_id);
-	}
-};
+PROTOCOL_CLASS_SIMPLE(AutoSolveStartedMessage, protocol::PROTO_AUTOSOLVE);
