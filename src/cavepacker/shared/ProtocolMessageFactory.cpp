6bdec7424826194690c9a323f33ab6f009f4c2b4
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-08-01 08:31:29 +0200

    * auto solve network messages => added to the factory

--- src/cavepacker/shared/ProtocolMessageFactory.cpp
+++ src/cavepacker/shared/ProtocolMessageFactory.cpp
@@ -1,5 +1,7 @@
 #include "engine/common/network/ProtocolMessageFactory.h"
 #include "engine/common/Logger.h"
+#include "cavepacker/shared/network/ProtocolMessageTypes.h"
+#include "cavepacker/shared/network/messages/AutoSolveStartedMessage.h"
 
 ProtocolMessageFactory::ProtocolMessageFactory ()
 {
@@ -9,9 +11,15 @@ IProtocolMessage *ProtocolMessageFactory::create (ByteStream& stream)
 {
 	const protocolId type = stream.readByte();
 	IProtocolMessage *msg = getForProtocolId(stream, type);
-	if (msg == nullptr) {
+	if (msg != nullptr)
+		return msg;
+	switch (type) {
+	case protocol::PROTO_AUTOSOLVE:
+		return new AutoSolveStartedMessage();
+	default:
 		error(LOG_NET, String::format("unknown module type given: %i", type));
 		stream.addByte(type, true);
+		break;
 	}
 	return msg;
 }
