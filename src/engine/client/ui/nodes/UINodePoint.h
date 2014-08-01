dc33455285e50d55ed1b4d67b4d5d0baaf276050
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-08-01 20:38:24 +0200

    * ensure that the point node is updated at least once

--- src/engine/client/ui/nodes/UINodePoint.h
+++ src/engine/client/ui/nodes/UINodePoint.h
@@ -10,7 +10,7 @@ private:
 	uint32_t _updateDelay;
 public:
 	UINodePoint (IFrontend *frontend, uint32_t updateDelay = 10) :
-			UINodeLabel(frontend, "0", getFont(HUGE_FONT)), _current(0), _points(0), _lastUpdate(0), _updateDelay(
+			UINodeLabel(frontend, "0", getFont(HUGE_FONT)), _current(-1), _points(0), _lastUpdate(0), _updateDelay(
 					updateDelay)
 	{
 		Vector4Set(colorWhite, _fontColor);
@@ -23,14 +23,15 @@ public:
 			return;
 
 		const uint32_t passed = _time - _lastUpdate;
-		if (passed > _updateDelay) {
-			_lastUpdate += _updateDelay;
-			if (_current > _points)
-				--_current;
-			else
-				++_current;
-			setLabel(string::toString(_current));
-		}
+		if (passed < _updateDelay)
+			return;
+
+		_lastUpdate += _updateDelay;
+		if (_current > _points)
+			--_current;
+		else
+			++_current;
+		setLabel(string::toString(_current));
 	}
 
 	// this will increase the points over time. The given points value is an absolute value
@@ -43,7 +44,6 @@ public:
 	void addPoints (int points)
 	{
 		_points += points;
-		_current = clamp(_current, 0, _points);
 	}
 
 	// this sets the points and starts the increasing from the beginning. The give points value is an absolute value
@@ -51,8 +51,5 @@ public:
 	{
 		_current = 0;
 		_points = points;
-		_lastUpdate = _time;
-		if (points == 0)
-			setLabel("0");
 	}
 };
