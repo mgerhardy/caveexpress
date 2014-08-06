edbd6a85e4b29450a8e7a412843f1f9e42837141
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-08-06 08:05:47 +0200

    * disabled too much debug output

--- src/caveexpress/shared/network/ProtocolMessageFactory.cpp
+++ src/caveexpress/shared/network/ProtocolMessageFactory.cpp
@@ -18,7 +18,7 @@ ProtocolMessageFactory::ProtocolMessageFactory ()
 IProtocolMessage *ProtocolMessageFactory::create (ByteStream& stream)
 {
 	const protocolId type = stream.readByte();
-	debug(LOG_GENERAL, String::format("msg type => %i", (int)type));
+	//debug(LOG_GENERAL, String::format("msg type => %i", (int)type));
 	IProtocolMessage *msg = getForProtocolId(stream, type);
 	if (msg != nullptr)
 		return msg;
