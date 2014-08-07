3c90a5eb465a9fdd1d30e692aa592ca020ce79ea
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-08-07 21:31:28 +0200

    * draw gestures in the gesture window

--- src/engine/client/ui/windows/UIGestureWindow.cpp
+++ src/engine/client/ui/windows/UIGestureWindow.cpp
@@ -1,5 +1,6 @@
 #include "UIGestureWindow.h"
 #include "engine/common/FileSystem.h"
+#include "engine/common/Logger.h"
 
 #include <SDL.h>
 
@@ -7,8 +8,24 @@ UIGestureWindow::UIGestureWindow(IFrontend *frontend) :
 		UIWindow("gesture", frontend, WINDOW_FLAG_MODAL) {
 }
 
+void UIGestureWindow::render (int x, int y) const {
+	UIWindow::render(x, y);
+	for (CoordsConstIter i = _coords.begin(); i != _coords.end(); ++i) {
+		const vec2& v = *i;
+		_frontend->renderRect(x + v.x, y + v.y, 4, 4, colorRed);
+	}
+}
+
+bool UIGestureWindow::onFingerMotion (int64_t finger, uint16_t x, uint16_t y, int16_t dx, int16_t dy) {
+	const bool retVal = UIWindow::onFingerMotion(finger, x, y, dx, dy);
+	_coords.push_back(vec2(x, y));
+	return retVal;
+}
+
 void UIGestureWindow::onActive() {
 	UIWindow::onActive();
+
+	_coords.clear();
 
 	if (!SDL_RecordGesture(-1)) {
 		info(LOG_CLIENT, "Could not start gesture recording");
@@ -27,7 +44,7 @@ bool UIGestureWindow::onGestureRecord (int64_t gestureId)
 	SDL_RWops* rwops = SDL_RWFromFile(f->getURI().getPath().c_str(), "wb");
 	if (rwops) {
 		info(LOG_CLIENT, "Save gestures to " + f->getURI().getPath());
-		if (SDL_SaveAllDollarTemplates(rwops) <= 0) {
+		if (SDL_SaveDollarTemplate(gestureId, rwops) <= 0) {
 			info(LOG_CLIENT, "Failed to save gestures to " + f->getURI().getPath());
 		}
 		SDL_RWclose(rwops);
