b1374005541dacb8f4a60308f79bb8ee8ccbb58f
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-08-16 07:32:42 +0200

    * removed unused variable

--- src/engine/client/ui/windows/UIGestureWindow.cpp
+++ src/engine/client/ui/windows/UIGestureWindow.cpp
@@ -40,7 +40,6 @@ bool UIGestureWindow::onGestureRecord (int64_t gestureId)
 	info(LOG_CLIENT, "Save gestures");
 	const bool retVal = UIWindow::onGestureRecord(gestureId);
 	const URI uriLocal("file://" + FS.getAbsoluteWritePath() + "gesture-" + string::toString(gestureId));
-	std::string path;
 	const FilePtr& f = FS.getFile(uriLocal);
 	SDL_RWops* rwops = SDL_RWFromFile(f->getURI().getPath().c_str(), "wb");
 	if (rwops) {
