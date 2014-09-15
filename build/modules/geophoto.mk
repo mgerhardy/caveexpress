43560fa2f7ff24b2c42db45e2d98c27b5271aaeb
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-09-15 19:40:44 +0200

    * fixed some geophoto linking problems

--- build/modules/geophoto.mk
+++ build/modules/geophoto.mk
@@ -10,7 +10,7 @@ $(TARGET)_LINKER   := $(CXX)
 $(TARGET)_FILE     := $(TARGET)$(EXE_EXT)
 $(TARGET)_LDFLAGS  += $(SDL_LIBS) $(SDL_IMAGE_LIBS) $(SDL_MIXER_LIBS) $(OPENGL_LIBS) $(OGG_LIBS) $(VORBIS_LIBS) $(PNG_LIBS) $(NCURSES_LIBS) -lz $(SO_LIBS) $(LUA_LIBS) $(SQLITE3_LIBS) $(TINYXML2_LIBS) $(SDL_NET_LIBS) $(SDL_RWHTTP_LIBS) $(YAJL_LIBS)
 $(TARGET)_CFLAGS   += $(SDL_CFLAGS) $(SDL_IMAGE_CFLAGS) $(SDL_MIXER_CFLAGS) $(OPENGLES_CFLAGS) $(OGG_CFLAGS) $(VORBIS_CFLAGS) $(LUA_CFLAGS) $(PNG_CFLAGS) $(NCURSES_CFLAGS) $(SQLITE3_CFLAGS) $(TINYXML2_CFLAGS) $(SDL_NET_CFLAGS) $(SDL_RWHTTP_CFLAGS) $(YAJL_CFLAGS)
-$(TARGET)_SRCS      = $(subst $(SRCDIR)/,, \
+$(TARGET)_SRCS      = $(filter-out engine/client/ui/windows/UISettingsWindow.cpp,$(subst $(SRCDIR)/,, \
 	$(wildcard $(SRCDIR)/*.cpp) \
 	\
 	$(wildcard $(SRCDIR)/engine/client/shaders/*.cpp) \
@@ -43,12 +43,15 @@ $(TARGET)_SRCS      = $(subst $(SRCDIR)/,, \
 	$(wildcard $(SRCDIR)/geophoto/shared/*.cpp) \
 	$(wildcard $(SRCDIR)/geophoto/shared/network/*.cpp) \
 	) \
+	) \
 	\
 	$(SQLITE3_SRCS) \
 	\
 	$(LUA_SRCS) \
 	\
 	$(TINYXML2_SRCS) \
+	\
+	$(SDL_SRCS) \
 	\
 	$(SDL_IMAGE_SRCS) \
 	\
