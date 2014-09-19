e2ea933f9be9f247dccb47e90aaccfe133bb0c49
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-09-19 17:10:28 +0200

    * reverted parts of SDL

--- src/libs/SDL/src/audio/android/SDL_androidaudio.h
+++ src/libs/SDL/src/audio/android/SDL_androidaudio.h
@@ -30,8 +30,6 @@
 
 struct SDL_PrivateAudioData
 {
-    /* Resume device if it was paused automatically */
-    int resume;
 };
 
 static void AndroidAUD_CloseDevice(_THIS);
--- src/libs/SDL/src/video/android/SDL_androidevents.c
+++ src/libs/SDL/src/video/android/SDL_androidevents.c
@@ -29,11 +29,8 @@
 #include "SDL_events.h"
 #include "SDL_androidwindow.h"
 
-
 void android_egl_context_backup();
 void android_egl_context_restore();
-void AndroidAUD_ResumeDevices(void);
-void AndroidAUD_PauseDevices(void);
 
 void 
 android_egl_context_restore() 
@@ -77,14 +74,13 @@ Android_PumpEvents(_THIS)
     if (isPaused && !isPausing) {
         /* Make sure this is the last thing we do before pausing */
         android_egl_context_backup();
-        AndroidAUD_PauseDevices();
         if(SDL_SemWait(Android_ResumeSem) == 0) {
 #else
     if (isPaused) {
         if(SDL_SemTryWait(Android_ResumeSem) == 0) {
 #endif
             isPaused = 0;
-            AndroidAUD_ResumeDevices();
+            
             /* Restore the GL Context from here, as this operation is thread dependent */
             if (!SDL_HasEvent(SDL_QUIT)) {
                 android_egl_context_restore();
@@ -107,7 +103,6 @@ Android_PumpEvents(_THIS)
 #else
         if(SDL_SemTryWait(Android_PauseSem) == 0) {
             android_egl_context_backup();
-            AndroidAUD_PauseDevices();
             isPaused = 1;
         }
 #endif
