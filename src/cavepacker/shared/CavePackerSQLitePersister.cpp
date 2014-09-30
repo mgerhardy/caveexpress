03e856681ab6f17205a93e58c6cee75fe315728e
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-09-30 18:57:07 +0200

    * add a lives entry for the converted cavepacker campaign

--- src/cavepacker/shared/CavePackerSQLitePersister.cpp
+++ src/cavepacker/shared/CavePackerSQLitePersister.cpp
@@ -5,10 +5,12 @@ CavePackerSQLitePersister::CavePackerSQLitePersister (const std::string& filenam
 		SQLitePersister(filename)
 {
 	Transaction t(*_gameState);
-	if (_gameState->exec("UPDATE " TABLE_GAMEMAPS " SET campaignid = 'xsokoban', mapid = 'xsokoban' || mapid WHERE campaignid = 'tutorial' AND mapid LIKE '00%'"))
+	if (_gameState->exec("UPDATE " TABLE_GAMEMAPS " SET campaignid = 'xsokoban', mapid = 'xsokoban' || mapid WHERE campaignid = 'tutorial' AND mapid LIKE '00%'")) {
+		_gameState->exec("INSERT OR REPLACE INTO " TABLE_LIVES " (campaignid, lives, version) VALUES ('xsokoban', 3, '1');");
 		info(LOG_STORAGE, "updated the gamestate");
-	else
+	} else {
 		error(LOG_STORAGE, "failed to update the gamestate");
+	}
 }
 
 CavePackerSQLitePersister::~CavePackerSQLitePersister ()
