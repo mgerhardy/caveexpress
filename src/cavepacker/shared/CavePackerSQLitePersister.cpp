46ad39f50fab2162e095ac3ae97e4b699edfd5a5
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-09-30 18:52:05 +0200

    * move the update of the gamestate into a transaction

--- src/cavepacker/shared/CavePackerSQLitePersister.cpp
+++ src/cavepacker/shared/CavePackerSQLitePersister.cpp
@@ -4,6 +4,7 @@
 CavePackerSQLitePersister::CavePackerSQLitePersister (const std::string& filename) :
 		SQLitePersister(filename)
 {
+	Transaction t(*_gameState);
 	if (_gameState->exec("UPDATE " TABLE_GAMEMAPS " SET campaignid = 'xsokoban', mapid = 'xsokoban' || mapid WHERE campaignid = 'tutorial' AND mapid LIKE '00%'"))
 		info(LOG_STORAGE, "updated the gamestate");
 	else
