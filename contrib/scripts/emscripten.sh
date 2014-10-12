9d63186fb843a27a5e3d6e5763fd81f66c67134d
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-10-12 09:51:10 +0200

    * fixes on the emscripten setup script

--- contrib/scripts/emscripten.sh
+++ contrib/scripts/emscripten.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-EMSCRIPTEN_TARGET_ROOT=${1:-${HOME}}
-
 set -e
+
+EMSCRIPTEN_TARGET_ROOT=${1:-${HOME}}
+PREFIX=${EMSCRIPTEN_TARGET_ROOT}/emsdk/sdl2
 
 echo "Setup build environment for emscripten (install nodejs on your own)"
 cd ${EMSCRIPTEN_TARGET_ROOT}
@@ -16,30 +17,34 @@ cd emsdk
 ./emsdk install latest
 ./emsdk activate latest
 ./emsdk_env.sh
-sh ./emsdk_set_env.sh
-mkdir sdl-source && cd sdl-source
-	git clone https://github.com/Daft-Freak/SDL-emscripten.git
-	cd SDL-emscripten
-		emconfigure ./configure --host=asmjs-unknown-emscripten --disable-assembly --disable-threads --enable-cpuinfo=false CFLAGS="-O2" --prefix=${EMSCRIPTEN_TARGET_ROOT}/emsdk/sdl2
+source ./emsdk_set_env.sh
+mkdir -p sdl-source
+cd sdl-source
+rm -rf SDL-emscripten
+git clone https://github.com/Daft-Freak/SDL-emscripten.git
+cd SDL-emscripten
+	emconfigure ./configure --host=asmjs-unknown-emscripten --disable-assembly --disable-threads --enable-cpuinfo=false CFLAGS="-O2" --prefix=${PREFIX}
+	emmake make -j 4
+	make install
+cd ..
+rm -rf sdl2-image
+git clone https://github.com/gsathya/sdl2-image.git
+cd sdl2-image
+	./autogen.sh
+	emconfigure ./configure --disable-sdltest --with-sdl-prefix=${PREFIX} --prefix=${PREFIX}
+	emmake make -j 4
+	make install
+cd ..
+rm -rf SDL_mixer
+hg clone http://hg.libsdl.org/SDL_mixer/
+cd SDL_mixer
+	./autogen.sh
+	mkdir -p build && cd build
+		EMCONFIGURE_JS=1 emconfigure ../configure --disable-sdltest --with-sdl-prefix=${PREFIX} --prefix=${PREFIX}
 		emmake make -j 4
 		make install
-	cd ..
-	git clone https://github.com/gsathya/sdl2-image.git
-	cd sdl2-image
-		./autogen.sh
-		emconfigure ./configure --disable-sdltest --with-sdl-prefix=$(EMSCRIPTEN_TARGET_ROO)/emsdk/sdl2 --prefix=${EMSCRIPTEN_TARGET_ROOT}/emsdk/sdl2
-		emmake make -j 4
-		make install
-	cd ..
-	hg clone http://hg.libsdl.org/SDL_mixer/
-	cd SDL_mixer
-		./autogen.sh
-		mkdir build && cd build
-			EMCONFIGURE_JS=1 emconfigure ../configure --disable-sdltest --with-sdl-prefix=${EMSCRIPTEN_TARGET_ROOT}/emsdk/sdl2 --prefix=$(EMSCRIPTEN_TARGET_ROO)/emsdk/sdl2
-			emmake make -j 4
-			make install
-		cd ..
 	cd ..
 cd ..
+cd ..
 echo "Add the following line to your ~/.bashrc"
-echo "source $(EMSCRIPTEN_TARGET_ROOT)/emsdk/emsdk_set_env.sh
+echo "source ${EMSCRIPTEN_TARGET_ROOT}/emsdk/emsdk_set_env.sh
