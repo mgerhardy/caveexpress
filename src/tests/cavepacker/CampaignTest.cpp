5b0728913c40c67b6f6c2b16d33bdff352ee12d4
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-11-19 19:11:55 +0100

    * added cavepacker campaign loading test

--- src/tests/cavepacker/CampaignTest.cpp
+++ src/tests/cavepacker/CampaignTest.cpp
@@ -6,3 +6,10 @@ TEST(CampaignTest, testNew) {
 	CavePackerSQLitePersister *p = new CavePackerSQLitePersister("test/cavepacker.new.test");
 	delete p;
 }
+
+TEST(CampaignTest, testLoad) {
+	SCOPED_TRACE("test/cavepacker.gamestate.sqlite");
+	SQLitePersister persister("test/cavepacker.gamestate.sqlite");
+	Campaign campaign("tutorial", &persister);
+	ASSERT_TRUE(campaign.loadProgress()) << "failed to load the campaign progress";
+}
