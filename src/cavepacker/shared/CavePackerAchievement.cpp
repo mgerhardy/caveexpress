4ddc978041ec1ea1a19147ff2cbfc9233bd6342d
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-12-11 20:00:57 +0100

    * added new cavepacker achievements

--- src/cavepacker/shared/CavePackerAchievement.cpp
+++ src/cavepacker/shared/CavePackerAchievement.cpp
@@ -3,8 +3,19 @@
 namespace Achievements {
 Achievement STARS_3("achievement_3_x_3_stars", true);
 Achievement STARS_10("achievement_10_x_3_stars", true);
+Achievement STARS_300("achievement_300_x_3_stars", true);
 Achievement PUZZLES_10("achievement_10_puzzles", true);
 Achievement PUZZLES_20("achievement_20_puzzles", true);
 Achievement PUZZLES_30("achievement_30_puzzles", true);
 Achievement PUZZLES_40("achievement_40_puzzles", true);
+Achievement PUZZLES_50("achievement_50_puzzles", true);
+Achievement PUZZLES_60("achievement_60_puzzles", true);
+Achievement PUZZLES_70("achievement_70_puzzles", true);
+Achievement PUZZLES_80("achievement_80_puzzles", true);
+Achievement PUZZLES_90("achievement_90_puzzles", true);
+Achievement PUZZLES_100("achievement_100_puzzles", true);
+Achievement FINISHED_TUTORIALS("achievement_finished_tutorials");
+Achievement FINISHED_XSOKOBAN("achievement_finished_xsokoban");
+Achievement FINISHED_GRIGORUSHA("achievement_finished_the_grigorusha_campaign");
+Achievement FINSIHED_KSOKOBAN("achievement_finsihed_the_ksokoban_campaign");
 }
--- src/cavepacker/shared/CavePackerAchievement.h
+++ src/cavepacker/shared/CavePackerAchievement.h
@@ -5,8 +5,19 @@
 namespace Achievments {
 extern Achievement STARS_3;
 extern Achievement STARS_10;
+extern Achievement STARS_300;
 extern Achievement PUZZLES_10;
 extern Achievement PUZZLES_20;
 extern Achievement PUZZLES_30;
 extern Achievement PUZZLES_40;
+extern Achievement PUZZLES_50;
+extern Achievement PUZZLES_60;
+extern Achievement PUZZLES_70;
+extern Achievement PUZZLES_80;
+extern Achievement PUZZLES_90;
+extern Achievement PUZZLES_100;
+extern Achievement FINISHED_TUTORIALS;
+extern Achievement FINISHED_XSOKOBAN;
+extern Achievement FINISHED_GRIGORUSHA;
+extern Achievement FINSIHED_KSOKOBAN;
 }
