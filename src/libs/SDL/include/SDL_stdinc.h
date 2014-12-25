0d9cadc6d2f171a19eed72249c7d8ac0cd7893e0
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-12-25 10:32:49 +0100

    * updated SDL to also support emscripten

--- src/libs/SDL/include/SDL_stdinc.h
+++ src/libs/SDL/include/SDL_stdinc.h
@@ -172,7 +172,7 @@ typedef uint64_t Uint64;
 #ifdef PRIs64
 #define SDL_PRIs64 PRIs64
 #elif defined(__WIN32__)
-#define SDL_PRIs64 "I64"
+#define SDL_PRIs64 "I64d"
 #else
 #define SDL_PRIs64 "lld"
 #endif
@@ -184,6 +184,24 @@ typedef uint64_t Uint64;
 #define SDL_PRIu64 "I64u"
 #else
 #define SDL_PRIu64 "llu"
+#endif
+#endif
+#ifndef SDL_PRIx64
+#ifdef PRIx64
+#define SDL_PRIx64 PRIx64
+#elif defined(__WIN32__)
+#define SDL_PRIx64 "I64x"
+#else
+#define SDL_PRIx64 "llx"
+#endif
+#endif
+#ifndef SDL_PRIX64
+#ifdef PRIX64
+#define SDL_PRIX64 PRIX64
+#elif defined(__WIN32__)
+#define SDL_PRIX64 "I64X"
+#else
+#define SDL_PRIX64 "llX"
 #endif
 #endif
 
