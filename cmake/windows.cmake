59f397aa911d08e5885fcbc168a976fccba0df41
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-06-03 17:11:52 +0200

    * started with a windows toolchain

---
+++ cmake/windows.cmake
@@ -0,0 +1,4 @@
+# define the standard link libraries
+set(CMAKE_CXX_STANDARD_LIBRARIES "kernel32.lib user32.lib gdi32.lib winspool.lib shell32.lib ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib dbghelp.lib wsock32.lib ws2_32.lib rpcrt4.lib wininet.lib")
+set(CMAKE_C_STANDARD_LIBRARIES "kernel32.lib user32.lib gdi32.lib winspool.lib shell32.lib ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib dbghelp.lib wsock32.lib ws2_32.lib rpcrt4.lib wininet.lib")
+
