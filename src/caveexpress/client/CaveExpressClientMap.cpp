0d7078b316805b8f43ddfab633381ed3296d54e2
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-06-18 20:23:37 +0200

    * japanese translation

--- src/caveexpress/client/CaveExpressClientMap.cpp
+++ src/caveexpress/client/CaveExpressClientMap.cpp
@@ -151,23 +151,26 @@ void CaveExpressClientMap::renderEnd (int x, int y) const
 		renderWater(x, y);
 		return;
 	}
-	_frontend->renderTarget(_target);
+	if (getWaterHeight() > 0.000001f) {
+		const int widthWater = getPixelWidth() * _zoom;
+		const int waterSurface = y + getWaterSurface() * _zoom;
+		const int waterGround = y + getWaterGround() * _zoom;
+		const int waterHeight = waterGround - waterSurface;
 
-#if 1
-	_frontend->bindTargetTexture(_target);
-	renderWater(x, y);
-	_frontend->unbindTargetTexture(_target);
-	_target = nullptr;
-#else
-	if (getWaterHeight() <= 0.000001f)
-		return;
-	const int widthWater = getPixelWidth() * _zoom;
-	const int waterSurface = y + getWaterSurface() * _zoom;
-	const int waterGround = y + getWaterGround() * _zoom;
-	const int waterHeight = waterGround - waterSurface;
-	const TexturePtr& texture = UI::get().loadTexture("water");
-	_frontend->renderImage(texture.get(), x, waterSurface, widthWater, waterHeight, 0, 0.5f);
-#endif
+		_frontend->renderLine(x, waterSurface - 1, x + widthWater, waterSurface - 1, waterLineColor);
+		int scissorHeight = waterSurface - 2;
+		int scissorX = _x;
+		int scissorY = _y;
+		int scissorWidth = _width;
+		info(LOG_CLIENT, String::format("x: %i, y: %i, w: %i, h: %i, waterSurface: %i",
+				scissorX, scissorY, scissorWidth, scissorHeight, waterSurface));
+		_frontend->enableScissor(scissorX, scissorY, scissorWidth, scissorHeight);
+		_frontend->renderTarget(_target);
+		_frontend->disableScissor();
+		_target = nullptr;
+	} else {
+		_frontend->renderTarget(_target);
+	}
 }
 
 void CaveExpressClientMap::start () {
