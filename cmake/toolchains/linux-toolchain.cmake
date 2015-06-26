e6612c35d73e67e93abc7ee5cf7817cf354ca7ba
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-06-26 17:04:59 +0200

    * removed define from linux toolchain file

--- cmake/toolchains/linux-toolchain.cmake
+++ cmake/toolchains/linux-toolchain.cmake
@@ -1,10 +1,6 @@
 include(CheckFunctionExists)
 include(CheckLibraryExists)
 
-if (NOT NETWORKING)
-	add_definitions(-DNONETWORK=1)
-endif()
-
 if (CMAKE_COMPILER_IS_GNUCXX)
     check_function_exists(__atomic_fetch_add_4 HAVE___ATOMIC_FETCH_ADD_4)
     if (NOT HAVE___ATOMIC_FETCH_ADD_4)
