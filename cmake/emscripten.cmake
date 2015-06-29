5394e3ae6cc044a3b0511076e8472b058c0a6077
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-06-29 21:19:45 +0200

    * started to work on emscripten toolchain
    * shamelessly adopted from fips

---
+++ cmake/emscripten.cmake
@@ -0,0 +1,2 @@
+set(UNITTESTS OFF)
+set(TOOLS OFF)
--- contrib/scripts/emscripten.sh
+++ contrib/scripts/emscripten.sh
@@ -1,49 +1,10 @@
 #!/bin/bash
 
-set -e
-set -x
+DIR=$(dirname $(readlink -f $0))
+cd $DIR/../..
+mkdir -p build-emscripten
+cd build-emscripten
+pwd
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/emscripten-toolchain.cmake
+make $*
 
-EMSCRIPTEN_TARGET_ROOT=${1:-${HOME}}
-PREFIX=${EMSCRIPTEN_TARGET_ROOT}/emsdk/sdl2
-NODEVERSION="v0.10.32"
-
-nodejs() {
-	if [ ! -d node ]; then
-		wget http://nodejs.org/dist/${NODEVERSION}/node-${NODEVERSION}-linux-x64.tar.gz
-		tar -xzf node-${NODEVERSION}-linux-x64.tar.gz
-		mv node-${NODEVERSION}-linux-x64 node
-	fi
-	export PATH=$PATH:${EMSCRIPTEN_TARGET_ROOT}/node/bin
-}
-
-emscripten() {
-	if [ ! -d emsdk ]; then
-		wget https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz
-		tar -xzf emsdk-portable.tar.gz
-		mv emsdk_portable emsdk
-	fi
-	pushd emsdk
-	./emsdk update
-	./emsdk install latest
-	./emsdk activate latest
-	./emsdk_env.sh
-	source ./emsdk_set_env.sh
-}
-
-emscripten_noupdate() {
-	pushd emsdk
-	./emsdk_env.sh
-	source ./emsdk_set_env.sh
-}
-
-pushd ${EMSCRIPTEN_TARGET_ROOT}
-
-echo "Setup build environment for emscripten"
-emscripten
-#emscripten_noupdate
-
-echo "install nodejs"
-nodejs
-
-echo "Add the following line to your ~/.bashrc"
-echo "source ${EMSCRIPTEN_TARGET_ROOT}/emsdk/emsdk_set_env.sh"
