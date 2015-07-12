eb8648d94ea604620f5981911f7010189a14e6fc
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-07-12 15:30:58 +0200

    * added moveable sprite type

--- src/miniracer/shared/MiniRacerSpriteType.cpp
+++ src/miniracer/shared/MiniRacerSpriteType.cpp
@@ -4,6 +4,7 @@ namespace miniracer {
 
 namespace SpriteTypes {
 SpriteType SOLID("solid");
+SpriteType MOVEABLE("moveable");
 SpriteType ROAD("road");
 SpriteType DECAL("decal");
 SpriteType VEHICLE("vehicle");
--- src/miniracer/shared/MiniRacerSpriteType.h
+++ src/miniracer/shared/MiniRacerSpriteType.h
@@ -6,6 +6,7 @@ namespace miniracer {
 
 namespace SpriteTypes {
 extern SpriteType SOLID;
+extern SpriteType MOVEABLE;
 extern SpriteType ROAD;
 extern SpriteType DECAL;
 extern SpriteType VEHICLE;
@@ -21,9 +22,16 @@ inline bool isRoad (const SpriteType& other)
 	return other == ROAD;
 }
 
+// put onto the ground - only graphical - no physics impact
 inline bool isDecal (const SpriteType& other)
 {
 	return other == DECAL;
+}
+
+// solid but the vehicle can move this object
+inline bool isMoveable (const SpriteType& other)
+{
+	return other == MOVEABLE;
 }
 
 inline bool isVehicle (const SpriteType& other)
@@ -31,6 +39,7 @@ inline bool isVehicle (const SpriteType& other)
 	return other == VEHICLE;
 }
 
+// vehicle is slower here
 inline bool isLand (const SpriteType& other)
 {
 	return other == LAND;
