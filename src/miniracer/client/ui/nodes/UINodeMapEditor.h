c0391788cd417333fab5fc0ddb53729b99d706c5
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-07-16 19:03:22 +0200

    * auto fill with land tiles and started to support layers

--- src/miniracer/client/ui/nodes/UINodeMapEditor.h
+++ src/miniracer/client/ui/nodes/UINodeMapEditor.h
@@ -18,6 +18,22 @@ public:
 	virtual ~UINodeMapEditor() {
 	}
 
+	void doClear () override {
+		IUINodeMapEditor::doClear();
+		SpriteDefPtr def;
+		for (auto i = SpriteDefinition::get().begin(); i != SpriteDefinition::get().end(); ++i) {
+			if (SpriteTypes::isLand(i->second->type))
+				def = i->second;
+		}
+		if (!def)
+			return;
+		for (int w = 0; w < _mapWidth; ++w) {
+			for (int h = 0; h < _mapHeight; ++h) {
+				placeTileItem (def, nullptr, w, h, getLayer(SpriteTypes::LAND), false, 0.0f);
+			}
+		}
+	}
+
 	virtual IMapContext* getContext(const std::string& mapname) override {
 		return new MiniRacerMapContext(mapname);
 	}
@@ -27,6 +43,16 @@ public:
 	}
 
 	virtual void renderPlayer(int x, int y) const override {
+	}
+
+	virtual MapEditorLayer getLayer (const SpriteType& type) const {
+		if (SpriteTypes::isLand(type))
+			return LAYER_BACKGROUND;
+		if (SpriteTypes::isDecal(type))
+			return LAYER_DECORATION;
+		if (SpriteTypes::isVehicle(type) || SpriteTypes::isMoveable(type))
+			return LAYER_FOREGROUND;
+		return LAYER_FOREGROUND;
 	}
 
 	virtual bool isMapTile(const SpriteType &type) const override {
