648c162a8b173aa62813671c96cad6a744d210f0
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-07-29 20:12:40 +0200

    * CMAKE: add configuration variables to gui

--- cmake/toolchains/linux-toolchain.cmake
+++ cmake/toolchains/linux-toolchain.cmake
@@ -1,6 +1,10 @@
 include(CheckFunctionExists)
 include(CheckLibraryExists)
 
+set(CMAKE_EXE_LINKER_FLAGS "")
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE "")
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG "")
+
 if (CMAKE_COMPILER_IS_GNUCXX)
     check_function_exists(__atomic_fetch_add_4 HAVE___ATOMIC_FETCH_ADD_4)
     if (NOT HAVE___ATOMIC_FETCH_ADD_4)
@@ -13,10 +17,10 @@ endif()
 
 check_library_exists(rt clock_gettime "time.h" HAVE_CLOCK_GETTIME)
 if (HAVE_CLOCK_GETTIME)
-    set(CLOCK_LIBS "rt")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lrt")
 endif()
 
-set(CMAKE_C_STANDARD_LIBRARIES ${CMAKE_DL_LIBS} ${CLOCK_LIBS})
+set(CMAKE_C_STANDARD_LIBRARIES ${CMAKE_DL_LIBS})
 
 set(CMAKE_C_FLAGS "-pthread -Wcast-qual -Wcast-align -Wpointer-arith -Wshadow -Wall -Wextra -Wreturn-type -Wwrite-strings -Wno-unused-parameter")
 set(CMAKE_C_FLAGS_RELEASE "-O3 -ftree-vectorize -msse3 -ffast-math -DNDEBUG -D_FORTIFY_SOURCE=2")
@@ -25,3 +29,13 @@ set(CMAKE_C_FLAGS_DEBUG "-O0 -DDEBUG=1 -ggdb")
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -std=c++11 -Wnon-virtual-dtor -fno-rtti -fno-exceptions")
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE}")
 set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG}")
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "Generic C++ Compiler Flags" FORCE)
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG}" CACHE STRING "C++ Debug Compiler Flags" FORCE)
+set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE}" CACHE STRING "C++ Release Compiler Flags" FORCE)
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "Generic C Compiler Flags" FORCE)
+set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG}" CACHE STRING "C Debug Compiler Flags" FORCE)
+set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE}" CACHE STRING "C Release Compiler Flags" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}" CACHE STRING "Generic Linker Flags" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS_DEBUG}" CACHE STRING "Debug Linker Flags" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE "${CMAKE_EXE_LINKER_FLAGS_RELEASE}" CACHE STRING "Release Linker Flags" FORCE)
