d82eb1d88d50cb6cd514667475068eee4a4bf742
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-07-30 09:08:29 +0200

    YAJL: added missing header with incorrect version

---
+++ src/libs/yajl/yajl_version.h
@@ -0,0 +1,13 @@
+#ifndef YAJL_VERSION_H_
+#define YAJL_VERSION_H_
+
+#include <yajl/yajl_common.h>
+
+#define YAJL_MAJOR 2
+#define YAJL_MINOR 0
+#define YAJL_MICRO 0
+
+#define YAJL_VERSION ((YAJL_MAJOR * 10000) + (YAJL_MINOR * 100) + YAJL_MICRO)
+
+#endif /* YAJL_VERSION_H_ */
+
