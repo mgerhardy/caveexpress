ca378501216e8dba9768e4c08a511cf870fc5090
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-07-31 08:43:27 +0200

    COMMON: don't even try to emulate c++11

--- src/modules/common/Compiler.h
+++ src/modules/common/Compiler.h
@@ -1,41 +1,5 @@
 #pragma once
 
-#define GCC_ATLEAST(major, minor) (defined __GNUC__ && (__GNUC__ > (major) || (__GNUC__ == (major) && __GNUC_MINOR__ >= (minor))))
-
-#ifndef __has_extension // clang feature detection
-#	ifdef __has_feature
-#		define __has_extension __has_feature
-#	else
-#		define __has_extension(x) 0
-#	endif
-#endif
-
-#define CXX11(gcc_major, gcc_minor, msc_ver, clang_feature) ( \
-	__cplusplus >= 201103L || \
-	(defined __GXX_EXPERIMENTAL_CXX0X__ && GCC_ATLEAST((gcc_major), (gcc_minor))) || \
-	(defined _MSC_VER && (msc_ver) != 0 && _MSC_VER >= (msc_ver)) || \
-	__has_extension(clang_feature) \
-)
-
-#if not CXX11(4, 7, 1400, cxx_override_control)
-#	define override
-#endif
-
-#if __cplusplus < 201103L
-#ifndef nullptr
-/* not typesafe as the real nullptr from c++11 */
-#define nullptr 0
-#endif
-#endif
-
-#if 0
-#ifdef DEBUG
-#define __forceinline inline
-#else
-#define __forceinline inline __attribute__((always_inline))
-#endif
-#endif
-
 #if !defined(__GNUC__)
 #define  __attribute__(x)
 #endif
