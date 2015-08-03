1a721d591004b7ea9902f445f6389b7ac1038516
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-08-03 12:44:54 +0200

    GFX: fixed opengl frontend compilation for platforms where fixed function pipeline is not supported

--- src/modules/gfx/GL1Frontend.cpp
+++ src/modules/gfx/GL1Frontend.cpp
@@ -12,8 +12,10 @@
 #define GL_GLEXT_PROTOTYPES
 #include <SDL_opengl.h>
 //#include <SDL_opengl_glext.h>
-#endif
 #include "GLShared.h"
+#else
+typedef unsigned int TexNum;
+#endif
 
 struct TextureData {
 	int unused;
