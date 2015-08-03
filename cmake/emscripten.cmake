61846626930b61638473fbf26670ca1810bdd047
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-08-03 13:21:39 +0200

    EMSCRIPTEN: added shell and javascript functions to linking

--- cmake/emscripten.cmake
+++ cmake/emscripten.cmake
@@ -1,3 +1,5 @@
 set(UNITTESTS OFF)
 set(TOOLS OFF)
 set(USE_BUILTIN ON)
+
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --shell-file ${ROOT_DIR}/contrib/installer/html5/shell.html --js-library ${ROOT_DIR}/contrib/installer/html5/library.js")
