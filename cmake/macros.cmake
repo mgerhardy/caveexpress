4c4202d997731801ab7ed673cff2684e8e65d637
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-08-03 16:57:24 +0200

    EMSCRIPTEN: preload-file

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -537,6 +537,7 @@ macro(cp_add_executable)
 		set_target_properties(${_EXE_TARGET} PROPERTIES PROFILING_POSTFIX .html)
 		set_target_properties(${_EXE_TARGET} PROPERTIES RELEASE_POSTFIX .html)
 		set_target_properties(${_EXE_TARGET} PROPERTIES DEBUG_POSTFIX .html)
+		set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --preload-file ${ROOT_DIR}/base/${_EXE_TARGET}")
 	endif()
 
 	include_directories(${CMAKE_CURRENT_BINARY_DIR})
