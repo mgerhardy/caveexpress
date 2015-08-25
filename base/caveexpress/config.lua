mgerhardy/caveexpress

commit 6f76ca64177d47ecd2d58bc70ea5fd51ae7ff75a

GFX: hopefully fixes issue #26

src/modules/gfx/SDLFrontend.cpp
@@ -542,6 +542,28 @@ int SDLFrontend::init (int width, int height, bool fullscreen, EventHandler &eve
 	if (height == -1)
 		height = displayMode.h;
 
+#ifdef __ANDROID__
+	Config.getConfigVar("red", "6")->setValue("6");
+	Config.getConfigVar("green", "5")->setValue("5");
+	Config.getConfigVar("blue", "6")->setValue("6");
+#else
+	uint32_t r, g, b, a;
+	int bpp;
+	if (SDL_PixelFormatEnumToMasks(displayMode.format, &bpp, &r, &g, &b, &a)) {
+		const std::string& red = std::to_string(r);
+		const std::string& green = std::to_string(g);
+		const std::string& blue = std::to_string(b);
+
+		Config.getConfigVar("red", red)->setValue(red);
+		Config.getConfigVar("green", green)->setValue(green);
+		Config.getConfigVar("blue", blue)->setValue(blue);
+	} else {
+		Config.getConfigVar("red", "8")->setValue("8");
+		Config.getConfigVar("green", "8")->setValue("8");
+		Config.getConfigVar("blue", "8")->setValue("8");
+	}
+#endif
+
 	setGLAttributes();
 	setHints();
 
@@ -704,21 +726,12 @@ void SDLFrontend::setGLAttributes ()
 	sdlCheckError();
 	SDL_GL_SetAttribute(SDL_GL_RETAINED_BACKING, 1);
 	sdlCheckError();
-#ifdef __ANDROID__
-	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, Config.getConfigVar("red", "6")->getIntValue());
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, Config.getConfigVar("red", "")->getIntValue());
 	sdlCheckError();
-	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, Config.getConfigVar("green", "5")->getIntValue());
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, Config.getConfigVar("green", "")->getIntValue());
 	sdlCheckError();
-	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, Config.getConfigVar("blue", "6")->getIntValue());
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, Config.getConfigVar("blue", "")->getIntValue());
 	sdlCheckError();
-#else
-	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, Config.getConfigVar("red", "8")->getIntValue());
-	sdlCheckError();
-	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, Config.getConfigVar("green", "8")->getIntValue());
-	sdlCheckError();
-	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, Config.getConfigVar("blue", "8")->getIntValue());
-	sdlCheckError();
-#endif
 #ifdef __IPHONEOS__
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 	sdlCheckError();

commit dc5e5324f009271c64c073e1a431fc6f377cb87b

GFX: fixed color depth values that were broken in 6f76ca641 (you might need to clear your config files)
see issue #26

base/caveexpress/config.lua
@@ -16,6 +16,9 @@ defaultreferencetimefactor = 1.0
 defaultdamagethreshold = 3.0
 defaultnpcflyingspeed = 4.0
 defaultparticles = 100
+defaultred = 8
+defaultgreen = 8
+defaultblue = 8
 
 if isOUYA() then
 	defaultjoystick = true
@@ -23,6 +26,9 @@ if isOUYA() then
 	defaultdamagethreshold = 5.0
 	defaultnpcflyingspeed = 3.0
 	defaultparticles = 0
+	defaultred = 6
+	defaultgreen = 5
+	defaultblue = 6
 elseif isAndroid() then
 	defaultpersister = "googleplay"
 	defaultreferencetimefactor = 1.5
@@ -31,6 +37,9 @@ elseif isAndroid() then
 	defaultdamagethreshold = 5.0
 	defaultnpcflyingspeed = 3.0
 	defaultparticles = 50
+	defaultred = 6
+	defaultgreen = 5
+	defaultblue = 6
 elseif isMobile() then
 	defaultreferencetimefactor = 1.5
 	defaultshowcursor = false
@@ -73,6 +82,9 @@ settings = {
 	waterparticle = false,
 	npcflyingspeed = defaultnpcflyingspeed,
 	particles = defaultparticles,
+	red = defaultred,
+	green = defaultgreen,
+	blue = defaultblue,
 }
 
 controllerbindings = {

base/cavepacker/config.lua
@@ -14,15 +14,24 @@ defaultjoystick = false
 defaulttexturesize = "auto"
 defaultparticles = 100
 defaultfpslimit = 60
+defaultred = 8
+defaultgreen = 8
+defaultblue = 8
 
 if isOUYA() then
 	defaultjoystick = true
 	defaultparticles = 0
+	defaultred = 6
+	defaultgreen = 5
+	defaultblue = 6
 elseif isAndroid() then
 	defaultshowcursor = false
 	defaultparticles = 0
 	defaultfpslimit = 30
 	defaultpersister = "googleplay"
+	defaultred = 6
+	defaultgreen = 5
+	defaultblue = 6
 elseif isMobile() then
 	defaultshowcursor = false
 	defaultparticles = 0
@@ -58,6 +67,9 @@ settings = {
 	fpslimit = defaultfpslimit,
 	texturesize = defaulttexturesize,
 	particles = defaultparticles,
+	red = defaultred,
+	green = defaultgreen,
+	blue = defaultblue,
 }
 
 controllerbindings = {

base/miniracer/config.lua
@@ -14,15 +14,24 @@ defaultjoystick = false
 defaulttexturesize = "auto"
 defaultparticles = 100
 defaultfpslimit = 60
+defaultred = 8
+defaultgreen = 8
+defaultblue = 8
 
 if isOUYA() then
 	defaultjoystick = true
 	defaultparticles = 0
+	defaultred = 6
+	defaultgreen = 5
+	defaultblue = 6
 elseif isAndroid() then
 	defaultshowcursor = false
 	defaultparticles = 0
 	defaultfpslimit = 30
 	defaultpersister = "googleplay"
+	defaultred = 6
+	defaultgreen = 5
+	defaultblue = 6
 elseif isMobile() then
 	defaultshowcursor = false
 	defaultparticles = 0
@@ -58,6 +67,9 @@ settings = {
 	fpslimit = defaultfpslimit,
 	texturesize = defaulttexturesize,
 	particles = defaultparticles,
+	red = defaultred,
+	green = defaultgreen,
+	blue = defaultblue,
 }
 
 controllerbindings = {

src/cavepacker/server/map/deadlock/DeadlockDetector.cpp
@@ -65,6 +65,7 @@ bool DeadlockDetector::canMovePackageDirection(DeadlockState& state, char dir, i
 	getOppositeXY(dir, ox, oy);
 	if (!state.state.isFree(col + lx, row + ly)) {
 		if (state.state.isPackage(col + lx, row + ly)) {
+			state.checkedState.insert(std::make_pair(index, false));
 			const bool m = canMovePackage(state, col + lx, row + ly);
 			state.checkedState.insert(std::make_pair(index, m));
 			return m;

src/modules/common/ConfigManager.cpp
@@ -92,6 +92,9 @@ void ConfigManager::init (IBindingSpaceListener *bindingSpaceListener, int argc,
 	_grabMouse = getConfigValue(_configVarMap, "grabmouse", "true");
 	_soundEngine = getConfigValue(_configVarMap, "soundengine", "sdl");
 	_particles = getConfigValue(_configVarMap, "particles", "0");
+	getConfigValue(_configVarMap, "red", "8");
+	getConfigValue(_configVarMap, "green", "8");
+	getConfigValue(_configVarMap, "blue", "8");
 	_serverName = getConfigVar("servername", System.getCurrentUser());
 	_name = getConfigVar("name", System.getCurrentUser());
 	_amountOfFruitsForANewLife = getConfigValue(_configVarMap, "amountoffruitsforanewlife", "4");

src/modules/gfx/SDLFrontend.cpp
@@ -542,28 +542,6 @@ int SDLFrontend::init (int width, int height, bool fullscreen, EventHandler &eve
 	if (height == -1)
 		height = displayMode.h;
 
-#ifdef __ANDROID__
-	Config.getConfigVar("red", "6")->setValue("6");
-	Config.getConfigVar("green", "5")->setValue("5");
-	Config.getConfigVar("blue", "6")->setValue("6");
-#else
-	uint32_t r, g, b, a;
-	int bpp;
-	if (SDL_PixelFormatEnumToMasks(displayMode.format, &bpp, &r, &g, &b, &a)) {
-		const std::string& red = std::to_string(r);
-		const std::string& green = std::to_string(g);
-		const std::string& blue = std::to_string(b);
-
-		Config.getConfigVar("red", red)->setValue(red);
-		Config.getConfigVar("green", green)->setValue(green);
-		Config.getConfigVar("blue", blue)->setValue(blue);
-	} else {
-		Config.getConfigVar("red", "8")->setValue("8");
-		Config.getConfigVar("green", "8")->setValue("8");
-		Config.getConfigVar("blue", "8")->setValue("8");
-	}
-#endif
-
 	setGLAttributes();
 	setHints();
 
@@ -726,11 +704,15 @@ void SDLFrontend::setGLAttributes ()
 	sdlCheckError();
 	SDL_GL_SetAttribute(SDL_GL_RETAINED_BACKING, 1);
 	sdlCheckError();
-	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, Config.getConfigVar("red", "")->getIntValue());
+	const int r = Config.getConfigVar("red")->getIntValue();
+	const int g = Config.getConfigVar("green")->getIntValue();
+	const int b = Config.getConfigVar("blue")->getIntValue();
+	Log::info(LOG_CLIENT, "r: %i, g: %i, b: %i", r, g, b);
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, r);
 	sdlCheckError();
-	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, Config.getConfigVar("green", "")->getIntValue());
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, g);
 	sdlCheckError();
-	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, Config.getConfigVar("blue", "")->getIntValue());
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, b);
 	sdlCheckError();
 #ifdef __IPHONEOS__
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);

src/cavepacker/tests/BoardStateTest.cpp
@@ -167,7 +167,6 @@ TEST_F(BoardStateTest, testDeadlockByBlockedPackages) {
 	ASSERT_TRUE(s.hasDeadlock()) << "there is a deadlock by blocked packages";
 }
 
-
 TEST_F(BoardStateTest, testDeadlockNoDeadlock) {
 	BoardState s;
 