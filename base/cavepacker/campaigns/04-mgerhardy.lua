ef0da90bd873fac27738ea1fd6b97f4b07d4e677
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-08-30 14:07:40 +0200

    CAVEPACKER: new campaign

--- base/cavepacker/campaigns/04-mgerhardy.lua
+++ base/cavepacker/campaigns/04-mgerhardy.lua
@@ -1,11 +1,11 @@
 -- create a new campaign
-local c = Campaign.new("gri")
+local c = Campaign.new("mgerhardy")
 -- set an ingame icon for this campaign
-c:setSetting("icon", "gri-campaign")
+c:setSetting("icon", "mgerhardy-campaign")
 -- set the on screen message when this campaign is activated
-c:setSetting("text", "gri")
+c:setSetting("text", "mgerhardy")
 -- set the achievement id that you unlock once you finished this campaign
-c:setSetting("achievement", "achievement_finsihed_the_gri_campaign")
+c:setSetting("achievement", "achievement_finsihed_the_mgerhardy_campaign")
 -- now add all maps
-c:addMaps("gri*")
+c:addMaps("mgerhardy*")
 c:unlock()
