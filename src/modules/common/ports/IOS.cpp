39dfd329734fda140ab99135f25eb4be389eedd7
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-09-13 20:57:46 +0200

    COMMON: openURL for ios - but not yet active

--- src/modules/common/ports/IOS.cpp
+++ src/modules/common/ports/IOS.cpp
@@ -22,6 +22,21 @@ void IOS::showAds (bool show)
 
 int IOS::openURL (const std::string& url, bool) const
 {
+	// TODO: move in mm file
+#if 0
+	@autoreleasepool
+	{
+		UIApplication *app = [UIApplication sharedApplication];
+		NSURL *nsurl = [NSURL URLWithString:@(url.c_str())];
+
+		if ([app canOpenURL:nsurl]) {
+			const bool success = [app openURL:nsurl];
+			return success ? 0 : -1;
+		}
+		return -1;
+	}
+#endif
+
 	const std::string cmd = "open " + url;
 	system(cmd.c_str());
 	return 0;
