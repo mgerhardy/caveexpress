1ac47804b6c4a809e57f943d24011fd2859b7b74
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-09-15 21:20:21 +0200

    SCRIPTS: build the hd version of caveexpress

--- contrib/scripts/android.sh
+++ contrib/scripts/android.sh
@@ -4,6 +4,10 @@ DIR=$(dirname $(readlink -f $0))
 cd $DIR/../../..
 mkdir -p cp-build-android
 cd cp-build-android
-pwd
 cmake $DIR/../.. -DRELEASE=ON -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/android-toolchain.cmake
+make $*
+cd ..
+mkdir -p cp-build-android-hd
+cd cp-build-android-hd
+cmake $DIR/../.. -DRELEASE=ON -DMINIRACER=OFF -DCAVEPACKER=OFF -DHD_VERSION=ON -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/android-toolchain.cmake
 make $*
