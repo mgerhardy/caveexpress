e511bafcdf7bcb11f2e748b44bf2437aa73711a8
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-10-09 07:08:44 +0200

    SCRIPTS: allow to override release builds

--- contrib/scripts/android.sh
+++ contrib/scripts/android.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+RELEASE=${ANDROID_RELEASE:-ON}
 DIR=$(dirname $(readlink -f $0))
+echo $RELEASE
 cd $DIR/../../..
 mkdir -p cp-build-android
 cd cp-build-android
-cmake $DIR/../.. -DRELEASE=ON -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/android-toolchain.cmake
+cmake $DIR/../.. -DRELEASE=${RELEASE} -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/android-toolchain.cmake
 make $*
 cd ..
 mkdir -p cp-build-android-hd
 cd cp-build-android-hd
-cmake $DIR/../.. -DRELEASE=ON -DMINIRACER=OFF -DCAVEPACKER=OFF -DHD_VERSION=ON -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/android-toolchain.cmake
+cmake $DIR/../.. -DRELEASE=${RELEASE} -DMINIRACER=OFF -DCAVEPACKER=OFF -DHD_VERSION=ON -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/android-toolchain.cmake
 make $*
