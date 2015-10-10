68fb8a1dc5caf424c02b53f60889bfcd151d0811
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-10-10 09:50:09 +0200

    GAME: changed log category to GAME

--- src/modules/game/IGame.cpp
+++ src/modules/game/IGame.cpp
@@ -8,19 +8,19 @@ int IGame::getMaxClients() {
 
 void IGame::onCampaignUnlock(Campaign* oldCampaign, Campaign* newCampaign) {
 	if (oldCampaign == nullptr) {
-		Log::debug(LOG_SERVER, "no old campaign available while unlocking %s", newCampaign->getId().c_str());
+		Log::debug(LOG_GAME, "no old campaign available while unlocking %s", newCampaign->getId().c_str());
 		return;
 	}
 
 	const std::string& id = oldCampaign->getSetting("achievement");
 	if (id.empty()) {
-		Log::debug(LOG_SERVER, "no achievement for unlocking %s", oldCampaign->getId().c_str());
+		Log::debug(LOG_GAME, "no achievement for unlocking %s", oldCampaign->getId().c_str());
 		return;
 	}
 
 	const Achievement& achievement = Achievement::getByName(id);
 	if (!achievement) {
-		Log::error(LOG_SERVER, "invalid achievement for unlocking %s => %s",  oldCampaign->getId().c_str(), id.c_str());
+		Log::error(LOG_GAME, "invalid achievement for unlocking %s => %s",  oldCampaign->getId().c_str(), id.c_str());
 		return;
 	}
 
