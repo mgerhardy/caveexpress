76d2a318b712c7c68e9826d493cd1ca939f6de48
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-10-20 07:01:11 +0200

    SCRIPTS: android.sh: skip tools and unittests

--- contrib/scripts/android.sh
+++ contrib/scripts/android.sh
@@ -3,14 +3,13 @@
 RELEASE=${ANDROID_RELEASE:-ON}
 INSTALL_PACKAGES=${ANDROID_INSTALL_PACKAGES:-OFF}
 DIR=$(dirname $(readlink -f $0))
-echo $RELEASE
 cd $DIR/../../..
 mkdir -p cp-build-android
 cd cp-build-android
-cmake $DIR/../.. -DANDROID_INSTALL_PACKAGES=${INSTALL_PACKAGES} -DRELEASE=${RELEASE} -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/android-toolchain.cmake
+cmake $DIR/../.. -DTOOLS=OFF -DUNITTESTS=OFF -DANDROID_INSTALL_PACKAGES=${INSTALL_PACKAGES} -DRELEASE=${RELEASE} -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/android-toolchain.cmake
 make $*
 cd ..
 mkdir -p cp-build-android-hd
 cd cp-build-android-hd
-cmake $DIR/../.. -DANDROID_INSTALL_PACKAGES=${INSTALL_PACKAGES} -DRELEASE=${RELEASE} -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/android-toolchain.cmake -DMINIRACER=OFF -DCAVEPACKER=OFF -DHD_VERSION=ON
+cmake $DIR/../.. -DTOOLS=OFF -DUNITTESTS=OFF -DANDROID_INSTALL_PACKAGES=${INSTALL_PACKAGES} -DRELEASE=${RELEASE} -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/android-toolchain.cmake -DMINIRACER=OFF -DCAVEPACKER=OFF -DHD_VERSION=ON
 make $*
