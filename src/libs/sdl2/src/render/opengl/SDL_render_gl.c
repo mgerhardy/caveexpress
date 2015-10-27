13541e154729f124fbe4331fbd1674dd0eb8b62f
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-10-27 21:07:20 +0100

    LIBS: fixed issue #45 (GFX: entity and sprite selectors don't render the items with SDLFrontend - GLFrontend works though
    Problem was in sdl: https://hg.libsdl.org/SDL/rev/1d13a878b066
    also see https://bugzilla.libsdl.org/show_bug.cgi?id=3117

--- src/libs/sdl2/src/render/opengl/SDL_render_gl.c
+++ src/libs/sdl2/src/render/opengl/SDL_render_gl.c
@@ -1049,10 +1049,7 @@ GL_UpdateClipRect(SDL_Renderer * renderer)
         if (renderer->target) {
             data->glScissor(renderer->viewport.x + rect->x, renderer->viewport.y + rect->y, rect->w, rect->h);
         } else {
-            int w, h;
-
-            SDL_GetRendererOutputSize(renderer, &w, &h);
-            data->glScissor(renderer->viewport.x + rect->x, (h - renderer->viewport.y - renderer->viewport.h) + rect->y, rect->w, rect->h);
+            data->glScissor(rect->x, renderer->viewport.h - rect->y - rect->h, rect->w, rect->h);
         }
     } else {
         data->glDisable(GL_SCISSOR_TEST);
