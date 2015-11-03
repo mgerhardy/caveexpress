fcf8a25d25654c2852006144bf282a5d1afab906
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-11-03 18:09:30 +0100

    CMAKE: ios stuff

--- contrib/scripts/ios.sh
+++ contrib/scripts/ios.sh
@@ -4,9 +4,12 @@ set -e
 set -x
 
 DIR=$(cd $(dirname $0); pwd)
+ARCHIVE_PATH="caveproductions.xcarchive"
+EXPORT_PATH="export"
 cd $DIR/../../..
 mkdir -p cp-build-ios
 cd cp-build-ios
 pwd
-/Applications/CMake.app/Contents/bin/cmake -DRELEASE=OFF -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/ios-toolchain.cmake -GXcode $DIR/../..
-xcodebuild -project caveproductions.xcodeproj -configuration Debug -target ALL_BUILD $*
+/Applications/CMake.app/Contents/bin/cmake -DRELEASE=ON -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/ios-toolchain.cmake -GXcode $DIR/../..
+xcodebuild build archive -sdk iphoneos -project caveproductions.xcodeproj -configuration Release -target ALL_BUILD $*
+xcodebuild -exportArchive -exportFormat IPA -archivePath "${ARCHIVE_PATH}" -exportPath "${EXPORT_PATH}" -exportProvisioningProfile ""
