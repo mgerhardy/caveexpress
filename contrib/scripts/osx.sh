fe1c6cf902e57c6e1adb6b2fe488d8b4bd3c45d9
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-11-05 09:02:13 +0100

    OSX: updated build script (but with some TODOs included

--- contrib/scripts/osx.sh
+++ contrib/scripts/osx.sh
@@ -8,5 +8,21 @@ cd $DIR/../../..
 mkdir -p cp-build-osx
 cd cp-build-osx
 pwd
+
+# TODO: the path is incorrect
+IOS_PROVISIONG_PROFILES_DIR="${HOME}/Library/MobileDevice/Provisioning Profiles/"
+
+# TODO: extension is incorrect
+RESULTS=$(grep -l "Martin Gerhardy" ${PROVISIONG_PROFILES_DIR}/*.mobileprovision)
+
 /Applications/CMake.app/Contents/bin/cmake -DCMAKE_BUILD_TYPE=Release -GXcode $DIR/../..
-xcodebuild build -project caveproductions.xcodeproj -configuration Release $* | xcpretty
+
+for i in caveexpress cavepacker; do
+	echo "==========================================="
+	echo "Building $i"
+	echo "==========================================="
+	echo ""
+	echo ""
+	xcodebuild build archive -project caveproductions.xcodeproj -configuration Release -archivePath $i.xcarchive -scheme $i | xcpretty
+	xcodebuild -exportArchive -exportFormat ipa -archivePath $i.xcarchive -exportPath $i.ipa -exportProvisioningProfile "${RESULTS}" | xcpretty
+done
