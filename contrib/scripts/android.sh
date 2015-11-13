d1fb9ab8720368c4fe46e9a270fa21468ec1be6a
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-11-13 21:07:11 +0100

    CMAKE: use the android cmake toolchain file

--- contrib/scripts/android.sh
+++ contrib/scripts/android.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 BUILD_TYPE=Release
-if [ $ANDROID_RELEASE == "OFF" ]; then
+if [ "$ANDROID_RELEASE" == "OFF" ]; then
 	BUILD_TYPE=Debug
 fi
 INSTALL_PACKAGES=${ANDROID_INSTALL_PACKAGES:-OFF}
 DIR=$(dirname $(readlink -f $0))
 cd $DIR/../../..
+echo "build non hd version"
 mkdir -p cp-build-android
 cd cp-build-android
 cmake $DIR/../.. -DTOOLS=OFF -DUNITTESTS=OFF -DANDROID_INSTALL_PACKAGES=${INSTALL_PACKAGES} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/android-toolchain.cmake
 make $*
 cd ..
+echo "build hd version"
 mkdir -p cp-build-android-hd
 cd cp-build-android-hd
 cmake $DIR/../.. -DTOOLS=OFF -DUNITTESTS=OFF -DANDROID_INSTALL_PACKAGES=${INSTALL_PACKAGES} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/android-toolchain.cmake -DMINIRACER=OFF -DCAVEPACKER=OFF -DHD_VERSION=ON
