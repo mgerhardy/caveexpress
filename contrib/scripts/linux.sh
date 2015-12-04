06341d080276b961690b4ed5419f8806f2834dbc
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-12-04 16:23:16 +0100

    TRAVIS: reduced verbosity

--- contrib/scripts/linux.sh
+++ contrib/scripts/linux.sh
@@ -6,7 +6,7 @@ mkdir -p cp-build-linux
 cd cp-build-linux
 BINDIR=$(pwd)
 #-DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE
-cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${BINDIR}/linux -DUSE_CCACHE=ON $DIR/../..
+cmake -G"Eclipse CDT4 - Unix Makefiles" -DMINIRACER=ON -DCMAKE_INSTALL_PREFIX=${BINDIR}/linux -DUSE_CCACHE=ON $DIR/../..
 make $*
 make install
 
