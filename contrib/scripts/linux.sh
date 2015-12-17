a1ce28d2f34eb068b428a92743c3c1220b487075
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-12-17 09:14:43 +0100

    SCRIPTS: disabled miniracer and ccache by default for the linux build script

--- contrib/scripts/linux.sh
+++ contrib/scripts/linux.sh
@@ -6,7 +6,8 @@ mkdir -p cp-build-linux
 cd cp-build-linux
 BINDIR=$(pwd)
 #-DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE
-cmake -G"Eclipse CDT4 - Unix Makefiles" -DMINIRACER=ON -DCMAKE_INSTALL_PREFIX=${BINDIR}/linux -DUSE_CCACHE=ON $DIR/../..
+cmake --version
+cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${BINDIR}/linux $DIR/../..
 make $*
 make install
 
