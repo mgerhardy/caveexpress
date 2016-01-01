de6f721776e44709f1e815ddf3e18e72724df609
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-01-01 14:49:17 +0100

    SCRIPT: switched from configure to cmake

--- contrib/scripts/steamruntime.sh
+++ contrib/scripts/steamruntime.sh
@@ -9,4 +9,4 @@ cd $DIR/../..
 APP=${1:-caveexpress}
 ARCH=amd64
 
-schroot --chroot steamrt_scout_${ARCH} -- ls && ./configure --app=$APP && make release64
+schroot --chroot steamrt_scout_${ARCH} -- ls && mkdir ../cp-build-steam && cd ../cp-build-steam && cmake ../caveexpress && make
