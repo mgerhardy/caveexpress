mgerhardy/caveexpress

commit 144ecb7c3863227db26cb154929534a11bbc10fe

COMMON: moved game specific config vars out of common

src/modules/common/ConfigManager.cpp
@@ -91,18 +91,7 @@ void ConfigManager::init (IBindingSpaceListener *bindingSpaceListener, int argc,
 	_debugui = getConfigVar("debugui", "false");
 	_debugEntity = getConfigVar("debugentity", "false", true);
 	getConfigVar("alreadyrated", "false", true);
-
-	// TODO: game specific - create in IGame
-	_maxHitpoints = getConfigValue(_configVarMap, "maxhitpoints", "100");
-	_damageThreshold = getConfigValue(_configVarMap, "damagethreshold", "0.3");
-	_referenceTimeFactor = getConfigValue(_configVarMap, "referencetimefactor", "1.0");
-	_fruitCollectDelayForANewLife = getConfigValue(_configVarMap, "fruitcollectdelayforanewlife", "15000");
-	_amountOfFruitsForANewLife = getConfigValue(_configVarMap, "amountoffruitsforanewlife", "4");
-	_fruitHitpoints = getConfigValue(_configVarMap, "fruithitpoints", "10");
-	_npcFlyingSpeed = getConfigValue(_configVarMap, "npcflyingspeed", "4.0");
-	_waterParticle = getConfigValue(_configVarMap, "waterparticle", "false", CV_READONLY);
-	_mode = getConfigValue(_configVarMap, "mode", "");
-	// end TODO
+	_mode = getConfigValue(_configVarMap, "mode", "0");
 
 	for (KeyValueMap::iterator i = _configVarMap.begin(); i != _configVarMap.end(); ++i) {
 		getConfigVar(i->first, i->second, true);

src/modules/common/ConfigManager.h
@@ -84,21 +84,13 @@ private:
 	ConfigVarPtr _name;
 	ConfigVarPtr _vsync;
 	ConfigVarPtr _textureSize;
-	ConfigVarPtr _referenceTimeFactor;
-	ConfigVarPtr _maxHitpoints;
-	ConfigVarPtr _damageThreshold;
-	ConfigVarPtr _fruitCollectDelayForANewLife;
-	ConfigVarPtr _amountOfFruitsForANewLife;
-	ConfigVarPtr _fruitHitpoints;
-	ConfigVarPtr _npcFlyingSpeed;
+
 	ConfigVarPtr _debugui;
-	ConfigVarPtr _mode;
 	ConfigVarPtr _serverName;
-	// server side water particles
-	ConfigVarPtr _waterParticle;
 	// client side particle amount
 	ConfigVarPtr _particles;
 	ConfigVarPtr _renderToTexture;
+	ConfigVarPtr _mode;
 
 	BindingSpace _bindingSpace;
 
@@ -159,24 +151,33 @@ public:
 	bool isNetwork () const;
 	const std::string& getTextureSize () const;
 	void setTextureSize (const std::string& textureSize) const;
-	float getReferenceTimeFactor () const;
-	int getMaxHitpoints () const;
-	float getDamageThreshold () const;
-	int getAmountOfFruitsForANewLife () const;
-	int getFruitHitpoints () const;
-	float getNpcFlyingSpeed () const;
-	int getFruitCollectDelayForANewLife () const;
-	bool useWaterParticles () const;
 	LogLevel getLogLevel() const;
+
+	inline bool isModeSelected () const {
+		return !_mode->getValue().empty();
+	}
+
+	inline bool isModeHard () const {
+		return _mode->getValue() == "hard";
+	}
+
+	inline bool isModeEasy () const {
+		return _mode->getValue() == "easy";
+	}
+
+	inline void setMode (const std::string& mode) {
+		_mode->setValue(mode);
+		_persister->save(_configVars);
+	}
 
 	int increaseCounter(const std::string& counterId);
 
-	bool isModeSelected () const;
-	bool isModeEasy () const;
-	bool isModeHard () const;
-	void setMode (const std::string& mode);
 	void setBindingsSpace (BindingSpace bindingSpace);
 	BindingSpace getBindingsSpace () const;
+
+	inline ConfigVarPtr initOrGetConfigVar (const std::string& name, const std::string& defaultValue = "", unsigned int flags = 0U) {
+		return getConfigValue(_configVarMap, name, defaultValue, flags);
+	}
 
 	inline BackendRendererData getDebugRenderer ()
 	{
@@ -375,70 +376,9 @@ inline void ConfigManager::setTextureSize (const std::string& textureSize) const
 	return _textureSize->setValue(textureSize);
 }
 
-inline float ConfigManager::getReferenceTimeFactor () const
-{
-	return _referenceTimeFactor->getFloatValue();
-}
-
-inline int ConfigManager::getMaxHitpoints () const
-{
-	return _maxHitpoints->getIntValue();
-}
-
-inline float ConfigManager::getDamageThreshold () const
-{
-	return _damageThreshold->getFloatValue();
-}
-
-inline int ConfigManager::getFruitCollectDelayForANewLife () const
-{
-	return _fruitCollectDelayForANewLife->getIntValue();
-}
-
-inline int ConfigManager::getAmountOfFruitsForANewLife () const
-{
-	return _amountOfFruitsForANewLife->getIntValue();
-}
-
-inline int ConfigManager::getFruitHitpoints () const
-{
-	return _fruitHitpoints->getIntValue();
-}
-
-inline float ConfigManager::getNpcFlyingSpeed () const
-{
-	return _npcFlyingSpeed->getFloatValue();
-}
-
-inline bool ConfigManager::isModeSelected () const
-{
-	return !_mode->getValue().empty();
-}
-
-inline bool ConfigManager::isModeHard () const
-{
-	return _mode->getValue() == "hard";
-}
-
-inline bool ConfigManager::isModeEasy () const
-{
-	return _mode->getValue() == "easy";
-}
-
-inline void ConfigManager::setMode (const std::string& mode)
-{
-	_mode->setValue(mode);
-	_persister->save(_configVars);
-}
-
 inline BindingSpace ConfigManager::getBindingsSpace () const
 {
 	return _bindingSpace;
-}
-
-inline bool ConfigManager::useWaterParticles () const
-{
-	return _waterParticle->getBoolValue();
 }
 
 inline int ConfigManager::getClientSideParticleMaxAmount () const

src/caveexpress/client/ui/windows/UIGameHelpWindow.cpp
@@ -32,7 +32,7 @@ UIGameHelpWindow::UIGameHelpWindow (IFrontend* frontend) :
 	addStoneFlyingHelp(panel);
 	addTreeHelp(panel);
 	// TODO: invisible and update on onActive
-	if (Config.isModeEasy() && Config.getAmountOfFruitsForANewLife() > 0) {
+	if (Config.isModeEasy() && Config.getConfigVar("amountoffruitsforanewlife")->getIntValue() > 0) {
 		addLivesHelp(panel);
 	}
 
@@ -147,7 +147,7 @@ void UIGameHelpWindow::addStoneFlyingHelp (UINode *panel)
 void UIGameHelpWindow::addLivesHelp (UINode *panel)
 {
 	UINode* hbox = createHPanel();
-	const int n = Config.getAmountOfFruitsForANewLife();
+	const int n = Config.getConfigVar("amountoffruitsforanewlife")->getIntValue();
 	for (int i = 0; i < n - 1; ++i) {
 		hbox->add(createSprite(EntityTypes::APPLE));
 	}

src/caveexpress/client/ui/windows/UIMapWindow.cpp
@@ -1,5 +1,16 @@
 #include "UIMapWindow.h"
 #include "caveexpress/client/ui/nodes/UINodeMap.h"
+#include "client/IMapControl.h"
+#include "ui/UI.h"
+#include "ui/nodes/UINodeButton.h"
+#include "ui/nodes/UINodeBar.h"
+#include "ui/nodes/UINodeSprite.h"
+#include "ui/nodes/UINodePoint.h"
+#include "ui/nodes/UINodeButtonText.h"
+#include "ui/layouts/UIHBoxLayout.h"
+#include "ui/windows/listener/OpenWindowListener.h"
+#include "common/ConfigManager.h"
+#include "campaign/persister/IGameStatePersister.h"
 
 namespace caveexpress {
 
@@ -7,6 +18,72 @@ UIMapWindow::UIMapWindow (IFrontend *frontend, ServiceProvider& serviceProvider,
 		IUIMapWindow(frontend, serviceProvider, campaignManager, new UINodeMap(frontend, serviceProvider, campaignManager, 0, 0, frontend->getWidth(), frontend->getHeight(), map))
 {
 	init();
+}
+
+void UIMapWindow::initHudNodes()
+{
+	const float barHeight = 12.0f / _frontend->getHeight();
+	const int spriteHeight = 30;
+	const float barWidth = 102.0f / _frontend->getWidth();
+	const int spriteNodeOffset = 15;
+
+	_panel = new UINode(_frontend);
+	_panel->setId("hudpanel");
+	UIHBoxLayout* layout = new UIHBoxLayout();
+	layout->setSpacing(0.02f);
+	_panel->setLayout(layout);
+	_panel->setStandardPadding();
+	_panel->setAlignment(NODE_ALIGN_TOP | NODE_ALIGN_CENTER);
+
+	UINodePoint* _points = new UINodePoint(_frontend, 150);
+	_points->setLabel("00000");
+	_points->setId(UINODE_POINTS);
+	_panel->add(_points);
+
+	UINodeBar* timeBar = new UINodeBar(_frontend);
+	timeBar->setId(UINODE_SECONDS_REMAINING);
+	const Color timeBarColor = { 1.0f, 1.0f, 1.0f, 0.5f };
+	timeBar->setSize(barWidth, barHeight);
+	timeBar->setBarColor(timeBarColor);
+	timeBar->setBorder(true);
+	timeBar->setBorderColor(colorWhite);
+	_panel->add(timeBar);
+
+	UINodeBar* hitpointsBar = new UINodeBar(_frontend);
+	hitpointsBar->setId(UINODE_HITPOINTS);
+	const int maxHitpoints = Config.getConfigVar("maxhitpoints")->getIntValue();
+	hitpointsBar->setMax(maxHitpoints);
+	hitpointsBar->setSize(barWidth, barHeight);
+	hitpointsBar->setBorder(true);
+	hitpointsBar->setBorderColor(colorWhite);
+	// TODO: wind indicator
+
+	_panel->add(hitpointsBar);
+
+	UINodeSprite* livesSprite = new UINodeSprite(_frontend, spriteHeight, spriteHeight);
+	livesSprite->setId(UINODE_LIVES);
+	livesSprite->setSpriteOffset(spriteHeight);
+	const SpritePtr sprite = UI::get().loadSprite("icon-heart");
+	for (uint8_t i = 0; i < INITIAL_LIVES; ++i) {
+		livesSprite->addSprite(sprite);
+	}
+	_panel->add(livesSprite);
+
+	UINodeSprite *targetCave = new UINodeSprite(_frontend, spriteHeight, spriteHeight);
+	targetCave->setId(UINODE_TARGETCAVEID);
+	targetCave->setImage("icon-targetcave");
+	_panel->add(targetCave);
+
+	UINodeSprite *collected = new UINodeSprite(_frontend, spriteHeight, spriteHeight);
+	collected->setId(UINODE_COLLECTED);
+	_panel->add(collected);
+
+	UINodeSprite* packagesSprite = new UINodeSprite(_frontend, spriteHeight, spriteHeight);
+	packagesSprite->setId(UINODE_PACKAGES);
+	packagesSprite->setSpriteOffset(spriteNodeOffset);
+	_panel->add(packagesSprite);
+
+	add(_panel);
 }
 
 }

src/caveexpress/client/ui/windows/UIMapWindow.h
@@ -7,6 +7,7 @@ namespace caveexpress {
 class UIMapWindow: public IUIMapWindow {
 public:
 	UIMapWindow (IFrontend *frontend, ServiceProvider& serviceProvider, CampaignManager& campaignManager, ClientMap& map);
+	virtual void initHudNodes() override;
 };
 
 }

src/caveexpress/main/CaveExpress.cpp
@@ -193,7 +193,7 @@ void CaveExpress::update (uint32_t deltaTime)
 	if (isDone && !_map.isRestartInitialized()) {
 		const uint32_t playTime = _map.getTime();
 		const uint32_t referenceTime = _map.getReferenceTime();
-		const float relativeRefTime = ConfigManager::get().getReferenceTimeFactor() * referenceTime;
+		const float relativeRefTime = Config.getConfigVar("referencetimefactor")->getFloatValue() * referenceTime;
 		// this max is needed for debug reasons (if you force a map win)
 		const float time = std::max(1.0f, playTime / 1000.0f);
 		const uint32_t timeSeconds = std::max(1U, playTime / 1000U);
@@ -280,6 +280,16 @@ void CaveExpress::shutdown ()
 
 void CaveExpress::init (IFrontend *frontend, ServiceProvider& serviceProvider)
 {
+	// TODO: move into constants
+	Config.initOrGetConfigVar("maxhitpoints", "100");
+	Config.initOrGetConfigVar("damagethreshold", "0.3");
+	Config.initOrGetConfigVar("referencetimefactor", "1.0");
+	Config.initOrGetConfigVar("fruitcollectdelayforanewlife", "15000");
+	Config.initOrGetConfigVar("amountoffruitsforanewlife", "4");
+	Config.initOrGetConfigVar("fruithitpoints", "10");
+	Config.initOrGetConfigVar("npcflyingspeed", "4.0");
+	Config.initOrGetConfigVar("waterparticle", "false", CV_READONLY);
+
 	ClientEntityRegistry &r = Singleton<ClientEntityRegistry>::getInstance();
 	r.registerFactory(&EntityTypes::DECORATION, ClientMapTile::FACTORY);
 	r.registerFactory(&EntityTypes::SOLID, ClientMapTile::FACTORY);
@@ -324,7 +334,7 @@ void CaveExpress::init (IFrontend *frontend, ServiceProvider& serviceProvider)
 
 	{
 		ExecutionTime e("loading persister");
-		const ConfigVarPtr& persister = Config.get().getConfigVar("persister", "sqlite", true, CV_READONLY);
+		const ConfigVarPtr& persister = Config.getConfigVar("persister", "sqlite", true, CV_READONLY);
 		if (persister->getValue() == "nop") {
 			_persister = new NOPPersister();
 		} else if (persister->getValue() == "googleplay" && System.supportGooglePlay()) {

src/caveexpress/server/entities/Player.cpp
@@ -24,11 +24,17 @@ const float gravityScale = 0.3f;
 
 Player::Player (Map& map, ClientId clientId) :
 		IEntity(EntityTypes::PLAYER, map), _touching(nullptr), _hitpoints(
-				Config.getMaxHitpoints()), _lives(0), _collectedNPC(nullptr), _acceleration(b2Vec2_zero), _fingerAcceleration(
+				0), _lives(0), _collectedNPC(nullptr), _acceleration(b2Vec2_zero), _fingerAcceleration(
 				false), _accelerateX(0), _accelerateY(0), _clientId(clientId), _lastAccelerate(
 				0), _name(""), _lastFruitCollected(0), _fruitsCollectedInARow(
 				0), _revoluteJoint(nullptr), _crashReason(CRASH_NONE) {
 	_godMode = Config.getConfigVar("godmode");
+	_maxHitPoints = Config.getConfigVar("maxhitpoints");
+	_hitpoints = _maxHitPoints->getIntValue();
+	_fruitHitPoints = Config.getConfigVar("fruithitpoints");
+	_damageThreshold = Config.getConfigVar("damagethreshold");
+	_amountOfFruitsForANewLife = Config.getConfigVar("amountoffruitsforanewlife");
+	_fruitCollectDelayForANewLife = Config.getConfigVar("fruitcollectdelayforanewlife");
 	setAnimationType(Animations::ANIMATION_IDLE);
 	setState(PlayerState::PLAYER_IDLE);
 	memset(_collectedEntities, 0, sizeof(_collectedEntities));
@@ -98,7 +104,7 @@ void Player::setAcceleration (int dx, int dy)
 void Player::subtractHitpoints (uint16_t hitpoints)
 {
 	const int oldHitpoints = _hitpoints;
-	const int maxHitpoints = Config.getMaxHitpoints();
+	const int maxHitpoints = _maxHitPoints->getIntValue();
 	_hitpoints = clamp(_hitpoints - hitpoints, 0, maxHitpoints);
 	if (oldHitpoints != _hitpoints) {
 		GameEvent.updateHitpoints(*this);
@@ -109,7 +115,7 @@ void Player::subtractHitpoints (uint16_t hitpoints)
 void Player::addHitpoints (uint16_t hitpoints)
 {
 	const int oldHitpoints = _hitpoints;
-	const int maxHitpoints = Config.getMaxHitpoints();
+	const int maxHitpoints = _maxHitPoints->getIntValue();
 	_hitpoints = clamp(oldHitpoints + hitpoints, 0, maxHitpoints);
 	if (oldHitpoints != _hitpoints) {
 		GameEvent.updateHitpoints(*this);
@@ -324,12 +330,12 @@ void Player::onPreSolve (b2Contact* contact, IEntity* entity, const b2Manifold* 
 	const b2Vec2& vA = bodyA->GetLinearVelocityFromWorldPoint(point);
 	const b2Vec2& vB = bodyB->GetLinearVelocityFromWorldPoint(point);
 	const float approachVelocity = fabs(b2Dot(vB - vA, worldManifold.normal));
-	const float damageThreshold = Config.getDamageThreshold();
+	const float damageThreshold = _damageThreshold->getFloatValue();
 	if (approachVelocity <= damageThreshold)
 		return;
 
 	const float factor = approachVelocity - damageThreshold;
-	const int maxHitpoints = Config.getMaxHitpoints();
+	const int maxHitpoints = _maxHitPoints->getIntValue();
 	const int hitpointReduceAmount = maxHitpoints / 10 * (1.0f + factor);
 	subtractHitpoints(hitpointReduceAmount);
 	Log::info(LOG_GAMEIMPL, "damageThreshold: %f, approachVelocity: %f, factor: %f, hitpointReduceAmount: %i",
@@ -372,11 +378,11 @@ bool Player::collect (CollectableEntity* entity)
 
 	const EntityType & entityType = entity->getType();
 	if (EntityTypes::isFruit(entityType) || EntityTypes::isEgg(entityType)) {
-		const uint32_t fruitCollectDelayMillis = Config.getFruitCollectDelayForANewLife();
+		const uint32_t fruitCollectDelayMillis = _fruitCollectDelayForANewLife->getIntValue();
 		if (_lastFruitCollected == 0 || _time - _lastFruitCollected < fruitCollectDelayMillis) {
 			_lastFruitCollected = _time;
 			++_fruitsCollectedInARow;
-			if (++_fruitsCollectedInARow == Config.getAmountOfFruitsForANewLife()) {
+			if (++_fruitsCollectedInARow == _amountOfFruitsForANewLife->getIntValue()) {
 				_fruitsCollectedInARow = 0;
 				_lastFruitCollected = 0;
 				if (Config.isModeEasy())
@@ -389,7 +395,7 @@ bool Player::collect (CollectableEntity* entity)
 		Achievements::PICK_UP_FRUIT.unlock();
 		Achievements::COLLECT_100_FRUITS.unlock();
 		_map.sendSound(ClientIdToClientMask(getClientId()), SoundTypes::SOUND_FRUIT_COLLECTED);
-		addHitpoints(Config.getFruitHitpoints());
+		addHitpoints(_fruitHitPoints->getIntValue());
 		return true;
 	}
 

src/caveexpress/server/entities/Player.h
@@ -64,6 +64,11 @@ private:
 	std::string _name;
 
 	ConfigVarPtr _godMode;
+	ConfigVarPtr _maxHitPoints;
+	ConfigVarPtr _fruitHitPoints;
+	ConfigVarPtr _damageThreshold;
+	ConfigVarPtr _amountOfFruitsForANewLife;
+	ConfigVarPtr _fruitCollectDelayForANewLife;
 
 	uint32_t _lastFruitCollected;
 	int8_t _fruitsCollectedInARow;

commit 7035f33fd3400c72b48700bd793669dd3e351bab

COMMON: fixed mode cvar
broken since 144ecb7c3863227db26cb154929534a11bbc10fe

src/modules/common/ConfigManager.cpp
@@ -91,7 +91,7 @@ void ConfigManager::init (IBindingSpaceListener *bindingSpaceListener, int argc,
 	_debugui = getConfigVar("debugui", "false");
 	_debugEntity = getConfigVar("debugentity", "false", true);
 	getConfigVar("alreadyrated", "false", true);
-	_mode = getConfigValue(_configVarMap, "mode", "0");
+	_mode = getConfigValue(_configVarMap, "mode", "");
 
 	for (KeyValueMap::iterator i = _configVarMap.begin(); i != _configVarMap.end(); ++i) {
 		getConfigVar(i->first, i->second, true);
@@ -395,4 +395,10 @@ int ConfigManager::increaseCounter (const std::string& counterId)
 	++current;
 	var->setValue(string::toString(current));
 	return current;
+}
+
+bool ConfigManager::isModeSelected () const
+{
+	const std::string& mode = _mode->getValue();
+	return mode == "hard" || mode == "easy";
 }

src/modules/common/ConfigManager.h
@@ -153,9 +153,7 @@ public:
 	void setTextureSize (const std::string& textureSize) const;
 	LogLevel getLogLevel() const;
 
-	inline bool isModeSelected () const {
-		return !_mode->getValue().empty();
-	}
+	bool isModeSelected () const;
 
 	inline bool isModeHard () const {
 		return _mode->getValue() == "hard";