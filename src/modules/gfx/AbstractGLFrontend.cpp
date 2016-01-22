279c0825959e37ff5807cd8ef43672d720182c9d
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-01-22 08:11:55 +0100

    GFX: fixed gl backend batch rendering

--- src/modules/gfx/AbstractGLFrontend.cpp
+++ src/modules/gfx/AbstractGLFrontend.cpp
@@ -544,7 +544,7 @@ void AbstractGLFrontend::renderEnd ()
 
 void AbstractGLFrontend::renderBatchBuffers()
 {
-	if (_currentBatch == 0)
+	if (_currentVertexIndex <= 0)
 		return;
 	SDL_assert_always(_batches[0].texnum != 0);
 
