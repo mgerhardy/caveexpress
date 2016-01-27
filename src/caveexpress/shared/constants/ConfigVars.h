b307f0ca8ea1c8779440eec531d359c8c3183099
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-01-27 19:58:46 +0100

    CAVEEXPRESS: added missing file

---
+++ src/caveexpress/shared/constants/ConfigVars.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define MAX_HITPOINTS "maxhitpoints"
+#define DAMAGE_THRESHOLD "damagethreshold"
+#define REFERENCE_TIME_FACTOR "referencetimefactor"
+#define FRUIT_COLLECT_DELAY_FOR_A_NEW_LIFE "fruitcollectdelayforanewlife"
+#define AMOUNT_OF_FRUITS_FOR_A_NEW_LIFE "amountoffruitsforanewlife"
+#define FRUIT_HITPOINTS "fruithitpoints"
+#define WATER_PARTICLE "waterparticle"
+#define NPC_FLYING_SPEED "npcflyingspeed"
+#define GOD_MODE "godmode"
