7a71a2588ffab0fd58a3004641b49925bca44179
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-02-03 17:21:43 +0100

    CAVEEXPRESS: extend help screen

--- src/caveexpress/client/ui/windows/intro/IntroFlying.cpp
+++ src/caveexpress/client/ui/windows/intro/IntroFlying.cpp
@@ -16,15 +16,18 @@ void IntroFlying::addIntroNodes(UINode* parent) {
 	parent->add(new IntroLabelHeadline(_frontend, tr("Objectives")));
 	parent->add(new IntroLabel(_frontend, tr("Just stay and watch what happens")));
 	parent->add(new IntroLabelHeadline(_frontend, tr("Hints")));
+	parent->add(new IntroLabel(_frontend, tr("The egg makes you invulverable")));
 	parent->add(new IntroLabel(_frontend, tr("Dropping stones onto dinos stuns them")));
 	if (System.hasTouch() && !System.isOUYA()) {
 		parent->add(new IntroLabel(_frontend, tr("Drop them with the second finger")));
 	} else {
 		parent->add(new IntroLabel(_frontend, tr("Drop them by hitting SPACE bar")));
+		parent->add(new IntroLabel(_frontend, tr("or A on your controller")));
 	}
 	parent->add(new IntroLabelHeadline(_frontend, tr("Description")));
 	parent->add(new IntroTypeDescription(parent, _frontend, EntityTypes::NPC_FLYING, Animations::ANIMATION_FLYING_RIGHT, tr("Pterodactyls")));
 	parent->add(new IntroTypeDescription(parent, _frontend, EntityTypes::STONE, Animations::ANIMATION_IDLE, tr("Stone")));
+	parent->add(new IntroTypeDescription(parent, _frontend, EntityTypes::EGG, Animations::ANIMATION_IDLE, tr("Egg")));
 }
 
 }
