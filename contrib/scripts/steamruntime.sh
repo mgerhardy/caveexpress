270084018b3413e4c3e31791074bb1231c2671be
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-02-19 07:01:01 +0100

    SCRIPTS: prepare steamruntime scripts for desura release

--- contrib/scripts/steamruntime.sh
+++ contrib/scripts/steamruntime.sh
@@ -3,10 +3,16 @@
 set -x
 set -e
 
+ARCH=${1:-amd64}
 DIR=$(dirname $(readlink -f $0))
-cd $DIR/../..
+cd $DIR/../../..
+mkdir -p cp-build-steam-${ARCH}
+cd cp-build-steam-${ARCH}
+BINDIR=$(pwd)
 
-APP=${1:-caveexpress}
-ARCH=amd64
-
-schroot --chroot steamrt_scout_${ARCH} -- ls && mkdir ../cp-build-steam && cd ../cp-build-steam && cmake ../caveexpress && make
+echo "#!/bin/sh" > ${BINDIR}/script.sh
+echo "cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${BINDIR}/steam $DIR/../.." >> ${BINDIR}/script.sh
+echo "make -j 4" >> ${BINDIR}/script.sh
+echo "make install" >> ${BINDIR}/script.sh
+chmod +x script.sh
+schroot --chroot steamrt_scout_${ARCH} -- ./script.sh
