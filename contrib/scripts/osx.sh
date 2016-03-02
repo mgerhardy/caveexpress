ec4e566f6305ec59edf241b8c0eba3d3e4678096
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-03-02 12:13:23 -0800

    SCRIPT: osx script install to get app bundles

--- contrib/scripts/osx.sh
+++ contrib/scripts/osx.sh
@@ -7,16 +7,8 @@ DIR=$(cd $(dirname $0); pwd)
 cd $DIR/../../..
 mkdir -p cp-build-osx
 cd cp-build-osx
-pwd
+BINDIR=$(pwd)
 
-/Applications/CMake.app/Contents/bin/cmake -DCMAKE_BUILD_TYPE=Release -GXcode $DIR/../..
+/Applications/CMake.app/Contents/bin/cmake -DCMAKE_INSTALL_PREFIX=${BINDIR} -DCMAKE_BUILD_TYPE=Release -GXcode $DIR/../..
 
-for i in caveexpress cavepacker; do
-	echo "==========================================="
-	echo "Building $i"
-	echo "==========================================="
-	echo ""
-	echo ""
-	xcodebuild build archive -project caveproductions.xcodeproj -configuration Release -archivePath $i.xcarchive -scheme $i | xcpretty
-	xcodebuild -exportArchive -exportFormat app -archivePath $i.xcarchive -exportPath ${i}App | xcpretty
-done
+xcodebuild build -target install -project caveproductions.xcodeproj -configuration Release | xcpretty
