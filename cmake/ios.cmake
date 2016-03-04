67aba17e8e092ba955e74960c9cf3bc3696a3550
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-03-04 08:04:10 -0800

    CMAKE: ios icons

--- cmake/ios.cmake
+++ cmake/ios.cmake
@@ -10,6 +10,35 @@ macro(cp_ios_prepare_content TARGET BUNDLED)
 	if (EXISTS ${ROOT_DIR}/cmake/${TARGET}.xcscheme)
 		configure_file(${ROOT_DIR}/cmake/${TARGET}.xcscheme ${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}.xcodeproj/xcshareddata/xcschemes/${TARGET}.xcscheme)
 	endif()
+	set(ICNS_SOURCE ${ROOT_DIR}/contrib/${TARGET}-icon-512x512.png)
+	if (EXISTS ${ICNS_SOURCE})
+		set(ICNS_TARGET ${CMAKE_BINARY_DIR}/${TARGET}.icns)
+		# TODO: sizes for ios?
+		set(ICNS_SIZES "16;32;64;128;256;512")
+		set(ICONSET_DIR ${CMAKE_BINARY_DIR}/${TARGET}.iconset)
+		message(STATUS "Writing iconset ${ICONSET_DIR}")
+		file(MAKE_DIRECTORY ${ICONSET_DIR})
+		# TODO: fimd sips and iconutil
+		foreach(ICNS_SIZE ${ICNS_SIZES})
+			add_custom_command(TARGET ${TARGET} POST_BUILD
+				COMMAND sips
+				ARGS -z ${ICNS_SIZE} ${ICNS_SIZE} ${ICNS_SOURCE} --out ${ICONSET_DIR}/icon_${ICNS_SIZE}x${ICNS_SIZE}.png
+				COMMENT "Create ${ICNS_SIZE}x${ICNS_SIZE} icon for ${TARGET}"
+			)
+			math(EXPR TARGETSIZE "${ICNS_SIZE} / 2")
+			add_custom_command(TARGET ${TARGET} POST_BUILD
+				COMMAND sips
+				ARGS -z ${ICNS_SIZE} ${ICNS_SIZE} ${ICNS_SOURCE} --out ${ICONSET_DIR}/icon_${TARGETSIZE}x${TARGETSIZE}x2.png
+				COMMENT "Create ${ICNS_SIZE}x${ICNS_SIZE}@2 icon for ${TARGET}"
+			)
+		endforeach()
+		add_custom_command(TARGET ${TARGET} POST_BUILD
+			COMMAND iconutil
+			ARGS -c icns -o ${ICNS_TARGET} ${ICONSET_DIR}
+			COMMENT "Create ${TARGET}.icns"
+		)
+		install(FILES ${ICNS_TARGET} DESTINATION ${TARGET}.app/Contents/Resources COMPONENT ${TARGET})
+	endif()
 endmacro()
 
 macro(cp_ios_generate_plist_file TARGET)
