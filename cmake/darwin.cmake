0eaa4baeee18c52bda51a8c1cce592075df7c647
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-03-03 22:52:13 -0800

    CMAKE: osx icon stuff

--- cmake/darwin.cmake
+++ cmake/darwin.cmake
@@ -6,6 +6,34 @@ macro(cp_osx_prepare_content TARGET BUNDLED)
 	if (EXISTS ${ROOT_DIR}/cmake/${TARGET}.xcscheme)
 		configure_file(${ROOT_DIR}/cmake/${TARGET}.xcscheme ${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}.xcodeproj/xcshareddata/xcschemes/${TARGET}.xcscheme)
 	endif()
+	set(ICNS_SOURCE ${ROOT_DIR}/contrib/${TARGET}-icon-512x512.png)
+	if (EXISTS ${ICNS_SOURCE})
+		set(ICNS_TARGET ${CMAKE_BINARY_DIR}/${TARGET}.icns)
+		set(ICNS_SIZES "16;32;64;128;256;512")
+		set(ICONSET_DIR ${CMAKE_BINARY_DIR}/${TARGET}.iconset)
+		message(STATUS "Writing iconset ${ICONSET_DIR}")
+		file(MAKE_DIRECTORY ${ICONSET_DIR})
+		# TODO: fimd sips and iconutil
+		foreach(ICNS_SIZE ${ICNS_SIZES})
+			add_custom_command(TARGET ${TARGET} POST_BUILD
+				COMMAND sips
+				ARGS -z ${ICNS_SIZE} ${ICNS_SIZE} ${ICNS_SOURCE} --out ${ICONSET_DIR}/icon_${ICNS_SIZE}x${ICNS_SIZE}.png
+				COMMENT "Create ${ICNS_SIZE}x${ICNS_SIZE} icon for ${TARGET}"
+			)
+			math(EXPR TARGETSIZE "${ICNS_SIZE} / 2")
+			add_custom_command(TARGET ${TARGET} POST_BUILD
+				COMMAND sips
+				ARGS -z ${ICNS_SIZE} ${ICNS_SIZE} ${ICNS_SOURCE} --out ${ICONSET_DIR}/icon_${TARGETSIZE}x${TARGETSIZE}x2.png
+				COMMENT "Create ${ICNS_SIZE}x${ICNS_SIZE}@2 icon for ${TARGET}"
+			)
+		endforeach()
+		add_custom_command(TARGET ${TARGET} POST_BUILD
+			COMMAND iconutil
+			ARGS -c icns -o ${ICNS_TARGET} ${ICONSET_DIR}
+			COMMENT "Create ${TARGET}.icns"
+		)
+		install(FILES ${ICNS_TARGET} DESTINATION ${TARGET}.app/Contents/MacOS COMPONENT ${TARGET})
+	endif()
 endmacro()
 
 macro(cp_osx_generate_plist_file TARGET)
@@ -15,6 +43,7 @@ macro(cp_osx_generate_plist_file TARGET)
 endmacro()
 
 macro(cp_osx_add_target_properties TARGET VERSION VERSION_CODE)
+	cp_message("Set mac parameters")
 	cp_set_properties(${TARGET} MACOSX_BUNDLE_LONG_VERSION_STRING "${VERSION}")
 	cp_set_properties(${TARGET} MACOSX_BUNDLE_SHORT_VERSION_STRING "${VERSION_CODE}")
 	cp_set_properties(${TARGET} MACOSX_BUNDLE_BUNDLE_VERSION "${VERSION_CODE}")
@@ -29,7 +58,6 @@ macro(cp_osx_add_target_properties TARGET VERSION VERSION_CODE)
 		set(CMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY "Mac Developer")
 	endif()
 	cp_set_properties(${TARGET} XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY ${CMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY})
-	cp_message("Set mac parameters")
 	if (${CMAKE_GENERATOR} STREQUAL "Xcode")
 		cp_set_properties(${TARGET} MACOSX_BUNDLE_EXECUTABLE_NAME \${EXECUTABLE_NAME})
 		cp_set_properties(${TARGET} MACOSX_BUNDLE_PRODUCT_NAME \${PRODUCT_NAME})
--- contrib/scripts/create_icns.sh
+++ contrib/scripts/create_icns.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-dir=${0%/*}
-if [ -d "$dir" ]; then
-  pushd "$dir"
-fi
-
-pushd ../../../cp-build-osx
-
 PROJECT=${1:-caveexpress}
 ICONDIR=$PROJECT.app/Contents/Resources/$PROJECT.iconset
 ORIGICON=../caveexpress/contrib/$PROJECT-icon-512x512.png
@@ -15,12 +8,12 @@ mkdir -p $ICONDIR
 
 # Normal screen icons
 for SIZE in 16 32 64 128 256 512; do
-	sips -z $SIZE $SIZE $ORIGICON --out $ICONDIR/icon_${SIZE}x${SIZE}.png
+sips -z $SIZE $SIZE $ORIGICON --out $ICONDIR/icon_${SIZE}x${SIZE}.png ;
 done
 
 # Retina display icons
 for SIZE in 16 32 64 128 256 512; do
-	sips -z $SIZE $SIZE $ORIGICON --out $ICONDIR/icon_$(expr $SIZE / 2)x$(expr $SIZE / 2)@2.png
+sips -z $SIZE $SIZE $ORIGICON --out $ICONDIR/icon_$(expr $SIZE / 2)x$(expr $SIZE / 2)x2.png
 done
 
 # Make a multi-resolution Icon
