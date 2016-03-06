b5dadde306fc937a2d6a0e7364915b160075fb0e
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-03-06 08:48:59 -0800

    SCRIPTS: same xcodebuild sequence as for the macosx script

--- contrib/scripts/ios.sh
+++ contrib/scripts/ios.sh
@@ -7,20 +7,8 @@ DIR=$(cd $(dirname $0); pwd)
 cd $DIR/../../..
 mkdir -p cp-build-ios
 cd cp-build-ios
-pwd
+BINDIR=$(pwd)
 
-IOS_PROVISIONG_PROFILES_DIR="${HOME}/Library/MobileDevice/Provisioning Profiles/"
+/Applications/CMake.app/Contents/bin/cmake -DCMAKE_INSTALL_PREFIX=${BINDIR} -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/ios-toolchain.cmake -GXcode $DIR/../..
 
-RESULTS=$(grep -l "Martin Gerhardy" "${IOS_PROVISIONG_PROFILES_DIR}"/*.mobileprovision)
-
-/Applications/CMake.app/Contents/bin/cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/ios-toolchain.cmake -GXcode $DIR/../..
-
-for i in caveexpress cavepacker; do
-	echo "==========================================="
-	echo "Building $i"
-	echo "==========================================="
-	echo ""
-	echo ""
-	xcodebuild build archive -sdk iphoneos -project caveproductions.xcodeproj -configuration Release -archivePath $i.xcarchive -scheme $i | xcpretty
-	xcodebuild -exportArchive -exportFormat ipa -archivePath $i.xcarchive -exportPath $i.ipa -exportProvisioningProfile "${RESULTS}" | xcpretty
-done
+xcodebuild build -target install -project caveproductions.xcodeproj -configuration Release | xcpretty
