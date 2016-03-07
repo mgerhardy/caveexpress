c6337c6c7f7f7fa92619c53a905bb499bf38b436
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-03-07 12:45:59 -0800

    SCRIPTS: disable pretty printing for ios compile script right now

--- contrib/scripts/ios.sh
+++ contrib/scripts/ios.sh
@@ -11,4 +11,4 @@ BINDIR=$(pwd)
 
 /Applications/CMake.app/Contents/bin/cmake -DCMAKE_INSTALL_PREFIX=${BINDIR} -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$DIR/../../cmake/toolchains/ios-toolchain.cmake -GXcode $DIR/../..
 
-xcodebuild build -target install -project caveproductions.xcodeproj -configuration Release | xcpretty
+xcodebuild build -target install -project caveproductions.xcodeproj -configuration Release
