19827bd3f66ac9966cab8c4b8d396bf8979a2e99
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-03-05 03:06:12 +0100

    TILED: started with example filees for the tiled mapeditor

---
+++ contrib/assets/tiled/caveexpress.tsx
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="caveexpress" tilewidth="1024" tileheight="1024" tilecount="17" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="480">
+  <image width="1024" height="1024" source="png/caveexpress/tile-rock-big-02-back-01.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="foobar" x="0" y="0" width="1024" height="1024"/>
+   <object id="2" x="0" y="0" width="1024" height="1024"/>
+  </objectgroup>
+ </tile>
+ <tile id="481">
+  <image width="1024" height="1024" source="png/caveexpress/tile-rock-big-01-back-01.png"/>
+ </tile>
+ <tile id="482">
+  <image width="512" height="512" source="png/caveexpress/tile-rock-DIR-04-back-01.png"/>
+ </tile>
+ <tile id="483">
+  <image width="512" height="512" source="png/caveexpress/tile-rock-01-back-01.png"/>
+ </tile>
+ <tile id="484">
+  <image width="512" height="512" source="png/caveexpress/tile-reference.png"/>
+ </tile>
+ <tile id="485">
+  <image width="512" height="512" source="png/caveexpress/tile-packagetarget-rock-01-idle-front-01.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="1" width="506" height="507"/>
+  </objectgroup>
+ </tile>
+ <tile id="486">
+  <image width="512" height="512" source="png/caveexpress/tile-lava-rock-DIR-01-back-01.png"/>
+ </tile>
+ <tile id="487">
+  <image width="512" height="512" source="png/caveexpress/tile-lava-ice-DIR-01-back-01.png"/>
+ </tile>
+ <tile id="488">
+  <image width="512" height="512" source="png/caveexpress/tile-ground-ledge-DIR-02-back-01.png"/>
+ </tile>
+ <tile id="489">
+  <image width="512" height="512" source="png/caveexpress/tile-ground-ledge-DIR-01-back-01.png"/>
+ </tile>
+ <tile id="490">
+  <image width="512" height="512" source="png/caveexpress/tile-ground-06-back-01.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-86" y="-66" width="638" height="164"/>
+   <object id="2" x="84" y="48" width="234" height="80">
+    <ellipse/>
+   </object>
+   <object id="3" x="-268" y="-78">
+    <ellipse/>
+   </object>
+   <object id="4" x="306" y="-4">
+    <ellipse/>
+   </object>
+   <object id="5" x="0" y="0" width="512" height="512"/>
+   <object id="6" x="0" y="0" width="512" height="512"/>
+   <object id="7" x="0" y="0" width="512" height="100"/>
+  </objectgroup>
+ </tile>
+ <tile id="491">
+  <image width="512" height="512" source="png/caveexpress/tile-ground-05-back-01.png"/>
+ </tile>
+ <tile id="492">
+  <image width="512" height="512" source="png/caveexpress/tile-ground-04-back-01.png"/>
+ </tile>
+ <tile id="493">
+  <image width="512" height="512" source="png/caveexpress/tile-ground-03-back-01.png"/>
+ </tile>
+ <tile id="494">
+  <image width="512" height="512" source="png/caveexpress/tile-ground-02-back-01.png"/>
+ </tile>
+ <tile id="495">
+  <image width="512" height="512" source="png/caveexpress/tile-ground-01-back-01.png"/>
+ </tile>
+ <tile id="496">
+  <image width="512" height="1024" source="png/caveexpress/tile-waterfall-01-back-01.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="512" width="512" height="512"/>
+  </objectgroup>
+ </tile>
+</tileset>
