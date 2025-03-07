bb5d2c0644f889554505d6c1fbe220474a876748
cryham <cryham@gmail.com>
2025-03-07 19:47:50 +0100

    gameplay: allow diving, DENSITY_PLAYER 1000

--- src/caveexpress/shared/constants/Density.h
+++ src/caveexpress/shared/constants/Density.h
@@ -12,7 +12,7 @@ namespace caveexpress {
 #define DENSITY_TREE			450.0f
 #define DENSITY_FRUIT			350.0f
 #define DENSITY_EGG				500.0f
-#define DENSITY_PLAYER			400.0f
+#define DENSITY_PLAYER			1000.0f  // 400.0f old, no diving
 #define DENSITY_AIR				1.2041f
 #define DENSITY_PACKAGETARGET	450.0f
 #define DENSITY_BRIDGE			1000.0f
