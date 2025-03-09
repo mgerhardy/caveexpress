a2402dc241a970bf382d25972d92182bc0be68d0
cryham <cryham@gmail.com>
2025-03-09 15:38:46 +0100

    add config var for _flyingSpeedX in map

--- base/caveexpress/config.lua
+++ base/caveexpress/config.lua
@@ -11,6 +11,7 @@ defaulttexturesize = "auto"
 defaultreferencetimefactor = 1.0
 defaultdamagethreshold = 3.0
 defaultnpcflyingspeed = 2.0
+defaultflyingspeedx = 1.0
 defaultparticles = 100
 defaultred = 8
 defaultgreen = 8
@@ -31,6 +32,7 @@ elseif isHTML5() then
 	defaultsoundengine = "dummy"
 	defaultnetwork = false
 else
+	defaultflyingspeedx = 2.5
 	defaultgamecontroller = true
 	defaultfrontend = "opengl3"
 	defaultparticles = 1000
@@ -60,6 +62,7 @@ settings = {
 	fruithitpoints = 10,
 	waterparticle = true,
 	npcflyingspeed = defaultnpcflyingspeed,
+	flyingspeedx = defaultflyingspeedx,
 	particles = defaultparticles,
 	red = defaultred,
 	green = defaultgreen,
