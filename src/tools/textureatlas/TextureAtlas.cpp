2201f4b79c7ec0acc35d490ae69baa681b7ce77b
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-03-09 19:24:16 +0100

    TEXTUREATLAS: allow to handle multiple input files

--- src/tools/textureatlas/TextureAtlas.cpp
+++ src/tools/textureatlas/TextureAtlas.cpp
@@ -25,6 +25,7 @@
 #include <stdlib.h>
 
 static const int ATLAS_INCREASE = 32;
+static bool debug = false;
 
 struct Image {
 	std::string name;
@@ -88,12 +89,31 @@ static std::string extractBasenameNoExtension(const std::string &in) {
 	return str;
 }
 
+// TODO: add gcc printf format checks
+static void debug_printf(const char *format, ...) {
+	if (debug) {
+		va_list args;
+		va_start(args, format);
+		printf("DEBUG: ");
+		vprintf(format, args);
+		va_end(args);
+	}
+}
+
+static void error_printf(const char *format, ...) {
+	va_list args;
+	va_start(args, format);
+	fprintf(stderr, "ERROR: ");
+	vfprintf(stderr, format, args);
+	va_end(args);
+}
+
 static double readDouble(const pugi::xml_node &parent, const std::string &name, double defaultVal = 0.0f) {
 	const std::string xpath = "./key[text()='" + name + "']/following-sibling::double";
 	pugi::xpath_node_set node = parent.select_nodes(xpath.c_str());
 	if (!node.empty()) {
 		double val = node.first().node().text().as_double();
-		printf("%s: %f\n", name.c_str(), val);
+		debug_printf("%s: %f\n", name.c_str(), val);
 		return val;
 	}
 	return defaultVal;
@@ -105,7 +125,7 @@ static std::string readString(const pugi::xml_node &node, const std::string &nam
 	pugi::xpath_node_set nodeSet = node.select_nodes(xpath.c_str());
 	if (!nodeSet.empty()) {
 		std::string val = nodeSet.first().node().text().as_string();
-		printf("%s: %s\n", name.c_str(), val.c_str());
+		debug_printf("%s: %s\n", name.c_str(), val.c_str());
 		return val;
 	}
 	return defaultVal;
@@ -166,14 +186,17 @@ static bool handleVariant(const Variants &v, const std::vector<Image> &images, c
 	std::vector<Image> scaledImages;
 	scaledImages.resize(images.size());
 
-	// Scale images with stb_image_resize2
+	printf("* Generate texture atlas for variant: %s (scale: %f)\n", v.extension.c_str(), v.scale);
+	printf("  * Save texture atlas at %s\n", v.textureFilename.c_str());
+	printf("  * Save lua file: %s\n", v.luaFilename.c_str());
+
 	for (size_t i = 0; i < images.size(); i++) {
 		const Image &image = images[i];
 		const int newWidth = (int)(image.width * v.scale);
 		const int newHeight = (int)(image.height * v.scale);
 		scaledImages[i].data = (uint8_t *)calloc(newWidth * newHeight * 4, 1);
 		if (!scaledImages[i].data) {
-			printf("Failed to allocate memory for scaled image\n");
+			error_printf("Failed to allocate memory for scaled image\n");
 			return false;
 		}
 		scaledImages[i].name = image.name;
@@ -187,16 +210,13 @@ static bool handleVariant(const Variants &v, const std::vector<Image> &images, c
 		} else {
 			stbir_resize_uint8_linear(image.data, image.width, image.height, inputImageStride, scaledImages[i].data,
 									  newWidth, newHeight, outputImageStride, STBIR_RGBA);
+			printf("  * Scaled image: %s, %i:%i -> %i:%i\n", image.name.c_str(), image.width, image.height, newWidth,
+				   newHeight);
 		}
-	}
-
-	for (size_t i = 0; i < scaledImages.size(); i++) {
 		rects[i].id = (int)i;
 		rects[i].w = scaledImages[i].width;
 		rects[i].h = scaledImages[i].height;
 	}
-
-	printf("use %s to save the texture atlas with a scale of %f\n", v.textureFilename.c_str(), v.scale);
 
 	int currentAtlasWidth = 256;
 	int currentAtlasHeight = 256;
@@ -210,7 +230,6 @@ static bool handleVariant(const Variants &v, const std::vector<Image> &images, c
 
 		// Pack rectangles
 		if (!stbrp_pack_rects(&ctx, rects.data(), (int)rects.size())) {
-			printf("Failed to pack rectangles with %i:%i\n", currentAtlasWidth, currentAtlasHeight);
 			if (++attempt % 2 == 0) {
 				currentAtlasWidth += ATLAS_INCREASE;
 			} else {
@@ -218,18 +237,19 @@ static bool handleVariant(const Variants &v, const std::vector<Image> &images, c
 			}
 			continue;
 		}
+		printf("  * Texture atlas resolution %i:%i\n", currentAtlasWidth, currentAtlasHeight);
 
 		// Create atlas
 		const size_t atlasMemSize = ((size_t)currentAtlasWidth * (size_t)currentAtlasHeight * 4);
 		uint8_t *atlas = (uint8_t *)calloc(atlasMemSize, 1);
 		if (!atlas) {
-			printf("Failed to allocate memory for atlas\n");
+			error_printf("Failed to allocate memory for atlas\n");
 			return false;
 		}
 
 		FILE *luaFile = fopen(v.luaFilename.c_str(), "w");
 		if (!luaFile) {
-			printf("Failed to open %s\n", v.luaFilename.c_str());
+			error_printf("Failed to open lua output file: %s\n", v.luaFilename.c_str());
 			return false;
 		}
 
@@ -275,17 +295,15 @@ static bool handleVariant(const Variants &v, const std::vector<Image> &images, c
 			fprintf(luaFile, "\t},\n");
 		}
 		fprintf(luaFile, "}\n");
-
 		fclose(luaFile);
 
 		// Save the atlas
 		stbi_write_png(v.textureFilename.c_str(), currentAtlasWidth, currentAtlasHeight, 4, atlas,
 					   currentAtlasWidth * 4);
-		printf("Texture atlas saved as %s\n", v.textureFilename.c_str());
-
 		free(atlas);
 		return true;
 	}
+	error_printf("Failed to pack texture atlas\n");
 	return false;
 }
 
@@ -293,14 +311,14 @@ static bool loadTps(const std::string &tpsFile) {
 	pugi::xml_document doc;
 	pugi::xml_parse_result result = doc.load_file(tpsFile.c_str());
 	if (!result) {
-		printf("Failed to load %s\n", tpsFile.c_str());
+		error_printf("Failed to load %s\n", tpsFile.c_str());
 		return false;
 	}
 
 	const pugi::xml_node &dataStructNode = doc.child("data").child("struct");
 	const std::string &textureFilename = readString(dataStructNode, "textureFileName", "", "filename");
 	if (textureFilename.empty()) {
-		printf("No textureFileName found\n");
+		error_printf("No textureFileName found\n");
 		return false;
 	}
 
@@ -310,7 +328,8 @@ static bool loadTps(const std::string &tpsFile) {
 
 	const pugi::xpath_node &luaNode = doc.select_node("//key[text()='lua']/following-sibling::struct/filename");
 	const std::string &luaFilename = luaNode.node().text().as_string();
-	printf("Lua file: %s\n", luaFilename.c_str());
+	printf("* Lua file template: %s\n", luaFilename.c_str());
+	printf("* Texture file template: %s\n", textureFilename.c_str());
 
 	pugi::xpath_node_set autoSDSettingsStructs =
 		doc.select_nodes("//key[text()='autoSDSettings']/following-sibling::array/struct");
@@ -334,18 +353,20 @@ static bool loadTps(const std::string &tpsFile) {
 			luaFilenameVariant.replace(pos, 3, extension);
 		}
 		variants.emplace_back(scale, extension, textureFilenameVariant, luaFilenameVariant);
+		printf("* Found variant: scale: %f, extension: %s\n", scale, extension.c_str());
 	}
 	if (variants.empty()) {
-		printf("No variants found\n");
+		error_printf("No variants found\n");
 		return false;
 	}
 
 	const pugi::xpath_node_set &tpsFiles =
 		doc.select_nodes("//key[text()='fileList']/following-sibling::array/filename");
 	std::vector<std::string> input_files;
+	printf("* Images:\n");
 	for (const pugi::xpath_node &file : tpsFiles) {
 		const std::string &imageName = file.node().text().as_string();
-		printf("Image file: %s\n", imageName.c_str());
+		printf("  * Image file: %s\n", imageName.c_str());
 		input_files.push_back(imageName);
 	}
 
@@ -356,7 +377,7 @@ static bool loadTps(const std::string &tpsFile) {
 		images[i].name = input_files[i];
 		images[i].data = stbi_load(input_files[i].c_str(), &images[i].width, &images[i].height, &images[i].channels, 4);
 		if (!images[i].data) {
-			printf("Failed to load %s\n", input_files[i].c_str());
+			error_printf("Failed to load %s\n", input_files[i].c_str());
 			return false;
 		}
 	}
@@ -373,29 +394,59 @@ static bool loadTps(const std::string &tpsFile) {
 	return true;
 }
 
+static void usage(const char *appname) {
+	fprintf(stderr, "Usage: %s [-d] [-h] [file.tps]...\n\n", appname);
+	fprintf(stderr, "This tool reads a TexturePacker .tps file and creates the texture atlas.\n");
+	exit(EXIT_FAILURE);
+}
+
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		printf("Usage: %s file.tps\n", argv[0]);
-		return 1;
+	const char *appname = argv[0];
+	size_t optsParsed = 1;
+	for (optsParsed = 1; optsParsed < argc && argv[optsParsed][0] == '-'; optsParsed++) {
+		switch (argv[optsParsed][1]) {
+		case 'd':
+			debug = true;
+			break;
+		case 'h':
+		default:
+			usage(appname);
+		}
 	}
-	const std::string tpsFile = argv[1];
 
-	// extract path
-	std::string path = tpsFile;
-	size_t pos = path.find_last_of("/\\");
-	if (pos != std::string::npos) {
-		path = path.substr(0, pos);
+	if (argc < 2) {
+		usage(appname);
 	}
 
-	// chdir to the path
-	if (chdir(path.c_str()) != 0) {
-		printf("Failed to chdir to %s\n", path.c_str());
-		return 1;
+	argv += optsParsed;
+	argc -= (int)optsParsed;
+
+	if (argc == 0) {
+		usage(appname);
 	}
-	// extract the filename
-	std::string filename = tpsFile.substr(pos + 1);
-	if (!loadTps(filename)) {
-		return 1;
+
+	for (int i = 0; i < argc; i++) {
+		const std::string tpsFile = argv[0];
+		printf("Processing %s\n", tpsFile.c_str());
+
+		// extract path
+		std::string path = tpsFile;
+		size_t pos = path.find_last_of("/\\");
+		if (pos != std::string::npos) {
+			path = path.substr(0, pos);
+		}
+
+		// chdir to the path
+		if (chdir(path.c_str()) != 0) {
+			error_printf("Failed to chdir to %s\n", path.c_str());
+			return 1;
+		}
+		// extract the filename
+		std::string filename = tpsFile.substr(pos + 1);
+		if (!loadTps(filename)) {
+			error_printf("Failed to handle %s\n", tpsFile.c_str());
+			return 1;
+		}
 	}
 	return 0;
 }
