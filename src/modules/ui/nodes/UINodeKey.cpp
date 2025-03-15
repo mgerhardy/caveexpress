82dd4cf8034ad879628e0967cd56f8b383522cc0
cryham <cryham@gmail.com>
2025-03-15 13:40:16 +0100

    editor help order, keys color, bigger font

--- src/modules/ui/nodes/UINodeKey.cpp
+++ src/modules/ui/nodes/UINodeKey.cpp
@@ -1,11 +1,15 @@
 #include "UINodeKey.h"
+#include "common/IFrontend.h"
+#include "common/Math.h"
 
 UINodeKey::UINodeKey (IFrontend *frontend, const std::string& keyName, float x, float y, float height) :
-		UINodeLabel(frontend, keyName)
+		UINodeLabel(frontend, "  "+keyName+"  ",
+			getFont(keyName.length() > 1 ? MEDIUM_FONT : HUGE_FONT))  // 1 char key bigger
 {
 	setBorder(true);
-	setBorderColor(colorBlack);
-	setBackgroundColor(colorWhite);
+	setBorderColor(colorGray);
+	setColor(colorWhite);
+	setBackgroundColor(colorBlack);
 
 	setPos(x, y);
 	setSize(0.0f, height);
