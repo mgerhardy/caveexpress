b358adc9821e2f3472fbd48d78ca2e8496c2b515
cryham <cryham@gmail.com>
2025-03-22 14:26:27 +0100

    hitpoints bar colors, red below 30, flash always

--- src/modules/client/network/UpdateHitpointsHandler.h
+++ src/modules/client/network/UpdateHitpointsHandler.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "common/Math.h"
 #include "network/IProtocolHandler.h"
 #include "network/messages/UpdateHitpointsMessage.h"
 #include "ui/UI.h"
@@ -11,6 +12,11 @@ public:
 	void execute (const UpdateHitpointsMessage* msg) override
 	{
 		UINodeBar* bar = UI::get().setBarValue(UI_WINDOW_MAP, UINODE_HITPOINTS, msg->getHitpoints());
-		bar->flash();
+		const bool red = msg->getHitpoints() < 30;
+		const bool yellow = msg->getHitpoints() < 60;
+		const Color& color =  red ? colorRed : yellow ? colorYellow : colorGreen;
+		bar->setBarColor(color);
+		bar->setBorderColor(color);
+		bar->flash(2000, red ? 2 : 1);
 	}
 };
